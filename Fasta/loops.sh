6f669432f0e51cd968a68c2b5cd16abb6608ffcc
adamscharlotte <adams@MacBook-Pro-3.local>
2022-07-22 16:19:52 +0200

    Update creating fasta code

--- Fasta/loops.sh
+++ Fasta/loops.sh
@@ -5,27 +5,3 @@ do
     IFS=';' read -r -a array <<< "$line"
     Rscript Fasta/create_fasta.R "${array[0]}"
 done
-
-name_file=/Users/adams/Projects/300K/2022-library-run/metadata/poolnames/AspN_names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    Rscript Fasta/create_fasta.R "${array[0]}"
-done
-
-name_file=/Users/adams/Projects/300K/2022-library-run/metadata/poolnames/HLA-I_names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    Rscript Fasta/create_fasta.R "${array[0]}"
-done
-
-name_file=/Users/adams/Projects/300K/2022-library-run/metadata/poolnames/LysN_names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    Rscript Fasta/create_fasta.R "${array[0]}"
-done
