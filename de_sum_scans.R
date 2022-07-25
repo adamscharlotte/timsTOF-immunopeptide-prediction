69def326a49fe1fb04a5b788fa7aa37822bf2386
adamscharlotte <adams@MacBook-Pro-3.local>
2022-07-25 16:11:09 +0200

    Create an info file where you van find the number of PSMs, ...

--- de_sum_scans.R
+++ de_sum_scans.R
@@ -2,25 +2,38 @@ library(tidyverse)
 library(data.table)
 
 base_path <- "/Users/adams/Projects/300K/2022-library-run/"
-pool <- "TUM_HLA_16"    # "TUM_aspn_1_2", "TUM_HLA_16", "TUM_lysn_5"
-plate <- "20220624_HLAI_1_96"    # 20220624_HLAI_1_96, 20220621_AspNlysN
+pool <- "TUM_HLA2_140"
+# "TUM_aspn_1_2", "TUM_HLA_16", TUM_lysn_5_no_enzyme, TUM_HLA2_122
+plate <- "20220623_HLAII_p2"
+# 20220624_HLAI_1_96, 20220621_AspNlysN, 20220623_HLAII_p2
+info_path <- paste(base_path, "MaxQuant/info.txt", sep = "")
 
 setwd(paste(base_path, "MaxQuant/", plate, "/", pool, "-txt/", sep = ""))
 list.files()
-
-tbl_info <- tibble(pool_name = pool, pool_PSMs = nrow(tbl_msms),
-    fasta_size = nrow(tbl_pool))
+getwd()
 
 tbl_msms <- fread("msms.txt") %>% as_tibble
-tbl_scans <- fread("accumulatedMsmsScans.txt") %>%
+tbl_pool_msms <- tbl_msms %>% filter(Proteins == pool)
+tbl_qc_msms <- tbl_msms %>% filter(stringr::str_starts(Proteins, "QC"))
+tbl_reverse_msms <- tbl_msms %>%
+    filter(!stringr::str_starts(Proteins, "QC")) %>%
+    filter(!Proteins == pool) %>%
+    select(Proteins, Sequence, Reverse)
+
+tbl_pool_scans <- fread("accumulatedMsmsScans.txt") %>%
     as_tibble %>%
-    filter(`Scan number` %in% tbl_msms$`Scan number`) %>%
+    filter(`Scan number` %in% tbl_pool_msms$`Scan number`) %>%
     mutate(Precursor = `PASEF precursor IDs`) %>%
     # Split the precursor ID and create a new row for each
     mutate(Precursor = strsplit(as.character(Precursor), ";")) %>%
     unnest(Precursor) %>%
     mutate(pool_name = pool) %>%
     unique()
+
+tbl_unique_pool_peptides <- tbl_pool_scans %>% select(Sequence) %>% unique()
+
+# This is also an option
+# tbl_pool_msms %>% select(Sequence) %>% unique()
 
 # The following resulted in more PSMs than in msms.txt
 # there seems to be another filter that is applied
@@ -34,19 +47,34 @@ tbl_pool <- tbl_meta %>%
     rename(Sequence_pool = Sequence) %>%
     unique()
 
-tbl_filtered_scans <- merge(tbl_scans, tbl_pool, by = "pool_name") %>%
+tbl_filtered_scans <- merge(tbl_pool_scans, tbl_pool, by = "pool_name") %>%
     as_tibble() %>%
     filter(stringr::str_ends(Sequence, trunc_sequence))
 
-tbl_pasef <- fread("pasefMsmsScans.txt") %>% as_tibble
-tbl_scans_pasef <- merge(tbl_filtered_scans, tbl_pasef, by = "Precursor") %>%
-    as_tibble()
+tbl_scans_contain <- merge(tbl_pool_scans, tbl_pool, by = "pool_name") %>%
+    as_tibble() %>%
+    filter(str_detect(Sequence_pool, Sequence))
+colnames(tbl_filtered_scans)
 
-tbl_scans_pasef %>% select(Sequence) %>% unique()
+tbl_scans_contain %>%
+    filter(!Sequence %in% tbl_filtered_scans$Sequence) %>%
+    select(Sequence, Sequence_pool, `Precursor retention time`) %>%
+    unique() %>%
+    print(n = 40)
 
-# Number of full-length sequences
 tbl_filtered_scans %>%
+    filter(Sequence_pool == "LTPEEEEILNKK") %>%
+    select(Sequence, Sequence_pool, `Precursor retention time`,
+    Modifications) %>%
+    unique()
+
+tbl_full_length <- tbl_filtered_scans %>%
     filter(Sequence == Sequence_pool) %>%
+    select(Sequence, trunc_sequence) %>%
+    unique()
+
+tbl_truncated <- tbl_filtered_scans %>%
+    filter(!Sequence %in% tbl_full_length$Sequence) %>%
     select(Sequence) %>%
     unique()
 
@@ -54,11 +82,22 @@ all_sequences <- tbl_filtered_scans %>%
     pull(Sequence) %>%
     unique()
 
-tbl_scans %>%
+tbl_wrong <- tbl_pool_scans %>%
     filter(!Sequence %in% all_sequences) %>%
     select(Sequence) %>%
     unique()
 
+tbl_info <- tibble(pool_name = pool, PSMs = nrow(tbl_msms),
+    pool_PSMs = nrow(tbl_pool_msms), qc_PSMs = nrow(tbl_qc_msms),
+    reverse_PSMs = nrow(tbl_reverse_msms),
+    pool_unique_peptides = nrow(tbl_unique_pool_peptides),
+    full_length = nrow(tbl_full_length),
+    truncated = nrow(tbl_truncated),
+    wrong_identification = nrow(tbl_wrong),
+    fasta_size = nrow(tbl_pool),
+    percentage = full_length / fasta_size * 100)
+
+fwrite(tbl_info, info_path, append = TRUE)
 # ----------------------------------------------------------------------------
 tbl_pool %>% filter(stringr::str_starts(Sequence, tbl_truncated$Sequence))
 
@@ -115,14 +154,35 @@ tbl_pool
 
 # -----------------------------------------------------------------------------
 library(timsr)
-base_path <- "/Users/adams/Projects/300K/2022-library-run/MaxQuant/LysN/"
-path <- paste(base_path, "AspNLysN-A5_S1-A5_1_6546.d", sep = "")
+
+tbl_double_precursor <- tbl_pool_scans %>%
+    select(Precursor) %>%
+    count(Precursor) %>%
+    filter(n > 1)
+
+tbl_pool_scans %>%
+    filter(Precursor %in% tbl_double_precursor$Precursor) %>%
+    select(Sequence, Precursor, `Scan number`, `Precursor retention time`) %>%
+    arrange(desc(Precursor))
+
+tbl_pasef <- fread("pasefMsmsScans.txt") %>% as_tibble
+tbl_scans_pasef <- merge(tbl_pool_scans, tbl_pasef, by = "Precursor") %>%
+    as_tibble()
+
+tbl_scans_pasef %>%
+    filter(Precursor %in% tbl_double_precursor$Precursor) %>%
+    select(Precursor, `Scan number`, Frame, ScanNumBegin, ScanNumEnd) %>%
+    arrange(desc(Precursor)) %>%
+    unique()
+
+base_path <- "/Users/adams/Projects/300K/2022-library-run/MaxQuant/"
+path <- paste(base_path, plate, "/HLAII_p2-D8_S1-D8_1_6777.d", sep = "")
 all_columns <- c("frame", "scan", "tof", "intensity", "retention_time")
 df <- TimsR(path) # get data handle
 print(df)
-df[2347]
+tbl_raw <- df[c(2449, 2451, 2115, 2114, 2610, 3247),
+    all_columns] %>% as_tibble()
+tbl_raw %>% pull(retention_time) %>% unique()
+tbl_raw %>% select(retention_time, frame) %>% unique()
 
-df_x <- df[c(1,5,67), all_columns]
-df_x <- df[13:354, 355]
 
-print(df_x)
