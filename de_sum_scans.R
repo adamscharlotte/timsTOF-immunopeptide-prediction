41e6ede0674cae7d64b650d76a1e1728a131f6ed
adamscharlotte <adams@MacBook-Pro-3.local>
2022-07-26 08:47:59 +0200

    Clean-up code

--- de_sum_scans.R
+++ de_sum_scans.R
@@ -33,7 +33,7 @@ tbl_pool_scans <- fread("accumulatedMsmsScans.txt") %>%
 tbl_unique_pool_peptides <- tbl_pool_scans %>% select(Sequence) %>% unique()
 
 # This is also an option
-# tbl_pool_msms %>% select(Sequence) %>% unique()
+# tbl_pool_msms %>% select(Sequence) %>% unique() # nolint
 
 # The following resulted in more PSMs than in msms.txt
 # there seems to be another filter that is applied
@@ -99,58 +99,8 @@ tbl_info <- tibble(pool_name = pool, PSMs = nrow(tbl_msms),
 
 fwrite(tbl_info, info_path, append = TRUE)
 # ----------------------------------------------------------------------------
-tbl_pool %>% filter(stringr::str_starts(Sequence, tbl_truncated$Sequence))
 
-# The frame and scan beginning and end can be found in tbl_scans_pasef
-# and can be mapped to get the raw scans.
-
-qc_path <- paste(base_path, "metadata/QC.txt", sep = "")
-tbl_qc <- fread(qc_path) %>% as_tibble
-
-tbl_scans_pasef %>% select(Precursor) %>% unique()
-tbl_scans_pasef %>% pull(`PASEF precursor IDs`)
-tbl_pasef %>% filter(!Precursor %in% tbl_scans$`PASEF precursor IDs`)
-tbl_msms_scans %>% filter(Sequence.x == Sequence.y)
-
-tbl_msms_scans %>%
-    select()
 tbl_single_scan <- tbl_pasef %>% count(Precursor) %>% filter(n == 1)
-
-tbl_msms %>% select(Proteins) %>% unique()
-tbl_msms %>% select(Sequence) %>% unique()
-tbl_msms %>% filter(`Scan number` == 5255)
-colnames(tbl_msms)
-tbl_msms %>%
-    select(Sequence, `Missed cleavages`) %>%
-    unique() %>%
-    count(`Missed cleavages`)
-
-tbl_scans %>% filter(`PASEF precursor IDs` == tbl_single_scan$Precursor)
-
-
-
-tbl_truncated <- tbl_msms %>%
-    select(Sequence) %>%
-    unique() %>%
-    filter(!Sequence %in% tbl_pool$Sequence)
-
-list_truncated <- tbl_truncated  %>% pull(Sequence) %>% paste(collapse = "|")
-
-
-# tbl_found_truncated <- (tbl_pool, grepl(list_truncated, Sequence))
-# tbl_found_truncated <- (tbl_truncated, grepl(list_truncated, Sequence))
-
-match <- str_match_all(tbl_pool$Sequence, list_truncated)
-head(match, n = 1000)
-tbl_truncated <- tbl_pool %>%
-    select(Sequence) %>%
-    unique() %>%
-    filter(str_detect(Sequence, tbl_msms$Sequence))
-
-tbl_found_truncated
-tbl_not_found <- tbl_truncated %>%
-
-tbl_pool
 
 # -----------------------------------------------------------------------------
 library(timsr)
@@ -185,4 +135,5 @@ tbl_raw <- df[c(2449, 2451, 2115, 2114, 2610, 3247),
 tbl_raw %>% pull(retention_time) %>% unique()
 tbl_raw %>% select(retention_time, frame) %>% unique()
 
+tbl_raw %>% filter(frame == 3247) %>% pull(retention_time)
 
