4a530d30b829a0f9c87d734c291ce7682e3422df
adamscharlotte <adams@MacBook-Pro-3.local>
2022-07-27 10:11:50 +0200

    In-silico cleavage to figure out what the max length is of the QC peptides

---
+++ in-silico-cleavage.R
@@ -0,0 +1,23 @@
+# BiocManager::install("cleaver") # nolint
+library("cleaver")
+library(tidyverse)
+library(data.table)
+
+jpt_qc_peptide <- "PEPTIDEKYVGDSYDSSAKHTAYSDFLSDKYGFSSEDIFTKVDTFLDGFSVKSILDYVSLVEKKPEPTIDE" # nolint
+jpt_rt_peptide <- "PEPTIDEKYSAHEEHHYDKHEHISSDYAGKTFAHTESHISKISLGEHEGGGKLSSGYDGTSYKTASGVGGFSTKFGTGTYAGGEKVGASTGYSGLKSYASDFGSSAKTLIAYDDSSTKLYSYYSSTESKLYTGAGYDEVKHLTGLTFDTYKGFLDYESTGAKFLASSEGGFTKYALDSYSLSSKFVGTEYDGLAKHFALFSTDVTKHDTVFGSYLYKALFSSITDSEKYFGYTSDTFGKTFTGTTDSFFKVSGFSDISIYKTFGTETFDTFKFLFTGYDTSVKASDLLSGYYIKGIFGAFTDDYKTSIDSFIDSYKVYAETLSGFIKGFVIDDGLITKGASDFLSFAVKVSSIFFDTFDKFFLTGTSIFVKGDFTFFIDTFKIDVYILALLLKFLISLLEEYFKLFISALVDFFKSILAFLYLYFKSLFFIIDGFVKSLIFFLSTLLKKPEPTIDE" # nolint
+prtc_rt_peptide <- "PEPTIDEKSSAAPPPPPRGISNEGQNASIKHVLTSIGEKDIPVPKPKIGDYAGIKTASEFDSAIAQDKSAAGAFGPELSRELGQSGVDTYLQTKGLILVGGYGTRGILFVGSGVSGGEEGARSFANQPLEVVYSKLTILEELRNGFILDGFPRELASGLSFPVGFKLSSEAPALFQFDLKKPEPTIDE" # nolint
+
+jpt_qc_peptide_c <- cleave(jpt_qc_peptide, enzym = "trypsin") %>% unname() %>% unlist() # nolint
+jpt_rt_peptide_c <- cleave(jpt_rt_peptide, enzym = "trypsin") %>% unname() %>% unlist() # nolint
+prtc_rt_peptide_c <- cleave(prtc_rt_peptide, enzym = "trypsin") %>% unname() %>% unlist() # nolint
+
+list_all_qc <- c(jpt_qc_peptide_c, jpt_rt_peptide_c,
+    prtc_rt_peptide_c)
+
+tbl_qc <- tibble(Sequence = list_all_qc) %>%
+    mutate(length = str_length(Sequence)) %>%
+    arrange(desc(length))
+
+min(tbl_qc$length)
+max(tbl_qc$length)
+
