7815923edc5d8560f7b009ecb68348bc1b74062c
adamscharlotte <adams@MacBook-Pro-3.local>
2022-09-14 15:52:24 +0200

    Add truncated peptides to QC_JPT_QC_Peptide

--- in-silico-cleavage.R
+++ in-silico-cleavage.R
@@ -11,16 +11,17 @@ jpt_rt_peptide_c <- cleave(jpt_rt_peptide, enzym = "trypsin") %>% unname() %>% u
 
 tbl_qc_qc <- tibble(Sequence = jpt_qc_peptide_c) %>%
     mutate(length = str_length(Sequence)) %>%
+    mutate(trunc_sequence = str_sub(Sequence, start = -7)) %>%
     mutate(Proteins = "QC_JPT_QC_Peptide")
 
 tbl_qc_rt <- tibble(Sequence = jpt_rt_peptide_c) %>%
     mutate(length = str_length(Sequence)) %>%
+    mutate(trunc_sequence = Sequence) %>%
     mutate(Proteins = "QC_JPT_RT_Peptide")
 
 tbl_full_qc <- rbind(tbl_qc_qc, tbl_qc_rt) %>%
     filter(length > 10) %>%
-    mutate(trunc_sequence = Sequence) %>%
-    select(Protein, Sequence, trunc_sequence)
+    select(Proteins, Sequence, trunc_sequence)
 
-qc_path <- "/Users/adams/Projects/300K/2022-library-run/metadata/qc-peptides.txt" # nolint
+qc_path <- "/Users/adams/Projects/300K/2022-library-run/Metadata/qc-peptides.txt" # nolint
 fwrite(tbl_full_qc, qc_path, col.names = TRUE, sep = "\t")
