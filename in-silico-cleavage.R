96bb453c86605a1dba96947c2ba06d1dd951bcf2
adamscharlotte <adams@MacBook-Pro-3.local>
2022-09-14 14:48:06 +0200

    Reformat the QC peptides

--- in-silico-cleavage.R
+++ in-silico-cleavage.R
@@ -3,22 +3,24 @@ library("cleaver")
 library(tidyverse)
 library(data.table)
 
-jpt_qc_peptide <- "PEPTIDEKYVGDSYDSSAKHTAYSDFLSDKYGFSSEDIFTKVDTFLDGFSVKSILDYVSLVEKKPEPTIDE" # nolint
-jpt_rt_peptide <- "PEPTIDEKYSAHEEHHYDKHEHISSDYAGKTFAHTESHISKISLGEHEGGGKLSSGYDGTSYKTASGVGGFSTKFGTGTYAGGEKVGASTGYSGLKSYASDFGSSAKTLIAYDDSSTKLYSYYSSTESKLYTGAGYDEVKHLTGLTFDTYKGFLDYESTGAKFLASSEGGFTKYALDSYSLSSKFVGTEYDGLAKHFALFSTDVTKHDTVFGSYLYKALFSSITDSEKYFGYTSDTFGKTFTGTTDSFFKVSGFSDISIYKTFGTETFDTFKFLFTGYDTSVKASDLLSGYYIKGIFGAFTDDYKTSIDSFIDSYKVYAETLSGFIKGFVIDDGLITKGASDFLSFAVKVSSIFFDTFDKFFLTGTSIFVKGDFTFFIDTFKIDVYILALLLKFLISLLEEYFKLFISALVDFFKSILAFLYLYFKSLFFIIDGFVKSLIFFLSTLLKKPEPTIDE" # nolint
+jpt_qc_peptide <- "KYVGDSYDSSAKHTAYSDFLSDKYGFSSEDIFTKVDTFLDGFSVKSILDYVSLVEKK" # nolint
+jpt_rt_peptide <- "KYSAHEEHHYDKHEHISSDYAGKTFAHTESHISKISLGEHEGGGKLSSGYDGTSYKTASGVGGFSTKFGTGTYAGGEKVGASTGYSGLKSYASDFGSSAKTLIAYDDSSTKLYSYYSSTESKLYTGAGYDEVKHLTGLTFDTYKGFLDYESTGAKFLASSEGGFTKYALDSYSLSSKFVGTEYDGLAKHFALFSTDVTKHDTVFGSYLYKALFSSITDSEKYFGYTSDTFGKTFTGTTDSFFKVSGFSDISIYKTFGTETFDTFKFLFTGYDTSVKASDLLSGYYIKGIFGAFTDDYKTSIDSFIDSYKVYAETLSGFIKGFVIDDGLITKGASDFLSFAVKVSSIFFDTFDKFFLTGTSIFVKGDFTFFIDTFKIDVYILALLLKFLISLLEEYFKLFISALVDFFKSILAFLYLYFKSLFFIIDGFVKSLIFFLSTLLKK" # nolint
 
 jpt_qc_peptide_c <- cleave(jpt_qc_peptide, enzym = "trypsin") %>% unname() %>% unlist() # nolint
 jpt_rt_peptide_c <- cleave(jpt_rt_peptide, enzym = "trypsin") %>% unname() %>% unlist() # nolint
 
-list_all_qc <- c(jpt_qc_peptide_c, jpt_rt_peptide_c)
-
-tbl_qc <- tibble(Sequence = list_all_qc) %>%
+tbl_qc_qc <- tibble(Sequence = jpt_qc_peptide_c) %>%
     mutate(length = str_length(Sequence)) %>%
-    arrange(desc(length))
+    mutate(Proteins = "QC_JPT_QC_Peptide")
 
-min(tbl_qc$length)
-max(tbl_qc$length)
+tbl_qc_rt <- tibble(Sequence = jpt_rt_peptide_c) %>%
+    mutate(length = str_length(Sequence)) %>%
+    mutate(Proteins = "QC_JPT_RT_Peptide")
 
-tbl_full_qc<- tbl_qc %>% filter(!str_detect(Sequence, "PEPTIDE"))
+tbl_full_qc <- rbind(tbl_qc_qc, tbl_qc_rt) %>%
+    filter(length > 10) %>%
+    mutate(trunc_sequence = Sequence) %>%
+    select(Protein, Sequence, trunc_sequence)
 
-qc_path <- "/Users/adams/Projects/300K/2022-library-run/metadata/qc-peptides.txt"
-fwrite(tbl_full_qc, qc_path, col.names=T, sep="\t")
+qc_path <- "/Users/adams/Projects/300K/2022-library-run/metadata/qc-peptides.txt" # nolint
+fwrite(tbl_full_qc, qc_path, col.names = TRUE, sep = "\t")
