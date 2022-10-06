e8f6d609d3c2461ca868096aad6a342adb04fa64
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-06 11:33:42 +0200

    Order the m/Z before annotation

--- Annotation/sum_annotate.py
+++ Annotation/sum_annotate.py
@@ -1,5 +1,6 @@
 # /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3
 
+from operator import concat
 import pandas as pd
 import numpy as np
 import os
@@ -19,19 +20,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument("pool", type=str)					# Filename
 args = parser.parse_args()
 
-# pool = "TUM_HLA_16"
+# pool = "TUM_HLA2_88"
+pool = args.pool
 
 base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
-sum_path = base_path + "precursor-consensus/summed/" + args.pool + ".csv"
-annot_path = base_path + "precursor-consensus/annotated-20ppm/" + args.pool + ".csv"
-
-# sum_path = base_path + "precursor-consensus/summed/" + pool + ".csv"
-# annot_path = base_path + "precursor-consensus/annotated/" + pool + ".csv"
+sum_path = base_path + "precursor-consensus/summed/" + pool + ".csv"
+annot_path = base_path + "precursor-consensus/annotated-20ppm/" + pool + ".csv"
 
 un_annot_df_combined = pd.read_csv(sum_path)
 
 un_annot_df_combined.MZ = un_annot_df_combined.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
 un_annot_df_combined.INTENSITIES = un_annot_df_combined.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
+
+# un_annot_df_combined[un_annot_df_combined["PRECURSOR"] == 8200][["INTENSITIES", "MZ"]].values
+# # un_annot_df_combined[un_annot_df_combined["PRECURSOR"] == 5323][["INTENSITIES", "MZ"]].values
+
+for i in range (len(un_annot_df_combined)):
+    zipped_list = zip(un_annot_df_combined.iloc[i]["MZ"], un_annot_df_combined.iloc[i]["INTENSITIES"])
+    sorted_pair = sorted(zipped_list)
+    tuples = zip(*sorted_pair)
+    list_1, list_2 = [ list(tuple) for tuple in  tuples]
+    un_annot_df_combined.at[i, "MZ"] = list_1
+    un_annot_df_combined.at[i, "INTENSITIES"] = list_2
 
 annot_df = annotate_spectra(un_annot_df_combined)
 full_df = pd.concat([un_annot_df_combined.drop(columns = ["INTENSITIES", "MZ"]), annot_df], axis=1)
@@ -39,3 +49,5 @@ full_df = pd.concat([un_annot_df_combined.drop(columns = ["INTENSITIES", "MZ"]),
 full_df["MZ"] = [';'.join(map(str, l)) for l in full_df['MZ']]
 full_df["INTENSITIES"] = [';'.join(map(str, l)) for l in full_df['INTENSITIES']]
 full_df.to_csv(annot_path)
+
+# full_df[full_df["PRECURSOR"] == 8200][["INTENSITIES", "MZ"]].values
