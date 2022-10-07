501d35fea6d72511015f0c7a84f9ec2dff06e383
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-07 11:13:25 +0200

    Generalize code

--- Plots/Mirror/write_mgf.py
+++ Plots/Mirror/write_mgf.py
@@ -7,19 +7,25 @@ import numpy as np
 import os
 from pyteomics import mgf
 
-# import argparse, pathlib
-# parser = argparse.ArgumentParser()
-# parser.add_argument('pool', type=str)					# Filename
-# args = parser.parse_args()
+import argparse, pathlib
+parser = argparse.ArgumentParser()
+parser.add_argument('csv_path', type=str)					# Filename
+parser.add_argument('mgf_path', type=str)					# Filename
+parser.add_argument('charge_name', type=str)					# Filename
+args = parser.parse_args()
 
-# pool = args.pool
-pool = "TUM_HLA2_88"
+csv_path = args.csv_path
+mgf_path = args.mgf_path
+charge_name = args.charge_name
 
-base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
-# csv_path = base_path + "full-truncated-qc/un-annotated/" + pool + ".csv"
-# mgf_path = base_path + "full-truncated-qc/un-annotated-mgf/" + pool + ".mgf"
-csv_path = base_path + "full-truncated-qc/annotated/" + pool + ".csv"
-mgf_path = base_path + "full-truncated-qc/annotated-mgf/" + pool + ".mgf"
+# base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
+# file_name = "TUM_HLA2_88"
+# csv_path = base_path + "full-truncated-qc/un-annotated/" + file_name + ".csv"
+# mgf_path = base_path + "full-truncated-qc/un-annotated-mgf/" + file_name + ".mgf"
+# csv_path = base_path + "full-truncated-qc/annotated-20ppm/" + file_name + ".csv"
+# mgf_path = base_path + "full-truncated-qc/annotated-mgf/" + file_name + ".mgf"
+charge_name = "CHARGE"
+charge_name = "PRECURSOR_CHARGE"
 
 csv_df = pd.read_csv(csv_path)
 
@@ -38,8 +44,7 @@ for i in range (len(csv_df)):
     title = frame + "_" + precursor
     mass = csv_df.iloc[i]["MASS"]
     retentiontime = csv_df.iloc[i]["RETENTION_TIME"]
-    # charge = csv_df.iloc[i]["CHARGE"]
-    charge = csv_df.iloc[i]["PRECURSOR_CHARGE"]
+    charge = csv_df.iloc[i][charge_name]
     mgf_params = dict({'title' : title, 'pepmass' : mass, 'rtinseconds' : retentiontime, 'charge' : charge})
     spectra_dict = dict({'m/z array' : mz_array, 'intensity array' : intensity_array, 'params' : mgf_params})
     spectra_list.append(spectra_dict)
@@ -47,15 +52,15 @@ for i in range (len(csv_df)):
 mgf.write(spectra = spectra_list, output = mgf_path)
 
 # ----------------------------------- PRECURSOR -----------------------------------
-# csv_path = base_path + "precursor-consensus/un-annotated/" + pool + ".csv"
+# csv_path = base_path + "precursor-consensus/summed/" + pool + ".csv"
 # mgf_path = base_path + "precursor-consensus/un-annotated-mgf/" + pool + ".mgf"
-csv_path = base_path + "precursor-consensus/annotated/" + pool + ".csv"
-mgf_path = base_path + "precursor-consensus/annotated-mgf/" + pool + ".mgf"
+# csv_path = base_path + "precursor-consensus/annotated/" + pool + ".csv"
+# mgf_path = base_path + "precursor-consensus/annotated-mgf/" + pool + ".mgf"
 
 csv_df = pd.read_csv(csv_path)
 
-# csv_df["INTENSITIES"] = csv_df.combined_INTENSITIES.str.split(';').apply(lambda s: [int(x) for x in s])
-# csv_df["MZ"] = csv_df.combined_MZ.str.split(';').apply(lambda s: [float(x) for x in s])
+csv_df["INTENSITIES"] = csv_df.combined_INTENSITIES.str.split(';').apply(lambda s: [int(x) for x in s])
+csv_df["MZ"] = csv_df.combined_MZ.str.split(';').apply(lambda s: [float(x) for x in s])
 
 csv_df.INTENSITIES = csv_df.INTENSITIES.str.split(';').apply(lambda s: [float(x) for x in s])
 csv_df.MZ = csv_df.MZ.str.split(';').apply(lambda s: [float(x) for x in s])
@@ -70,8 +75,7 @@ for i in range (len(csv_df)):
     title = precursor
     mass = csv_df.iloc[i]["MASS"]
     retentiontime = csv_df.iloc[i]["RETENTION_TIME"]
-    # charge = csv_df.iloc[i]["CHARGE"]
-    charge = csv_df.iloc[i]["PRECURSOR_CHARGE"]
+    charge = csv_df.iloc[i][charge_name]
     mgf_params = dict({'title' : title, 'pepmass' : mass, 'rtinseconds' : retentiontime, 'charge' : charge})
     spectra_dict = dict({'m/z array' : mz_array, 'intensity array' : intensity_array, 'params' : mgf_params})
     spectra_list.append(spectra_dict)
