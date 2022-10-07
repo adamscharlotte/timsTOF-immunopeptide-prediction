9daa38f70981e5bb481bb23139f6ab03df7a4cde
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-07 11:37:50 +0200

    More general and easier to use.

--- Plots/loss_plot.R
+++ Plots/loss_plot.R
@@ -2,27 +2,17 @@ library(tidyverse)
 library(data.table)
 library(ggplot2)
 
-fixed_s_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/log/log_fixed_scratch.csv"
-fixed_r_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/log/log_fixed_refinement.csv"
-un_callibrated_r_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/log/log_un_callibrated_refinement.csv"
+file_name <- "log_precursor_20ppm_calibrated_refinement"
+plot_title <- "Precursor - Calibrated CE - Refinement"
+log_base <- "/Users/adams/Projects/300K/2022-library-run/Annotation/log/"
+log_path <- paste(log_base, file_name, ".csv", sep = "")
 
-tbl_fixed_s <- fread(fixed_s_path) %>% as_tibble()
-tbl_fixed_r <- fread(fixed_r_path) %>% as_tibble()
-tbl_un_callibrated_r <- fread(un_callibrated_r_path) %>% as_tibble()
+tbl_log <- fread(log_path) %>% as_tibble()
 
-plot_s <- ggplot(tbl_fixed_s, aes(x=epoch, y=loss)) + 
+plot_log <- ggplot(tbl_log, aes(x = epoch, y = loss)) +
     geom_line() +
-    labs(title = "Training model from scratch - fixed CE")
+    labs(title = plot_title)
 
-plot_r <- ggplot(tbl_fixed_r, aes(x=epoch, y=loss)) + 
-    geom_line() +
-    labs(title = "Refining model - fixed CE")
-
-plot_uc_r <- ggplot(tbl_un_callibrated_r, aes(x=epoch, y=loss)) + 
-    geom_line() +
-    labs(title = "Refining model - original CE")
-
-
-ggsave("/Users/adams/Projects/300K/2022-library-run/Figures/log_fixed_scratch_plot.pdf", plot_s)
-ggsave("/Users/adams/Projects/300K/2022-library-run/Figures/log_fixed_refinement_plot.pdf", plot_r)
-ggsave("/Users/adams/Projects/300K/2022-library-run/Figures/log_un_callibrated_refinement_plot.pdf", plot_uc_r)
+plot_base <- "/Users/adams/Projects/300K/Results/Figures/Log/"
+plot_path <- paste(plot_base, file_name, ".pdf", sep = "")
+ggsave(plot_path, plot_log)
