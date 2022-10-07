e81659b2ebd779d470db2d2aebb6fb38857a166d
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-07 11:00:00 +0200

    Clean up the bash loops.

--- Annotation/annotation_loop.sh
+++ Annotation/annotation_loop.sh
@@ -1,12 +1,6 @@
-ssh cadams@10.152.135.57
-scp Annotation/create_annotation_input.R cadams@10.152.135.57:/home/cadams
-scp Annotation/filter_annotation_input.R cadams@10.152.135.57:/home/cadams
-scp Annotation/filter_and_sum.R cadams@10.152.135.57:/home/cadams
 
-# cd /Users/adams/Projects/300K/2022-library-run/metadata/poolnames
-# scp all-pool-names.txt cadams@10.152.135.57:/home/cadams
-# scp mapped_done.txt cadams@10.152.135.57:/home/cadams
-# scp Annotation/filter_and_add_ce.R cadams@10.152.135.57:/home/cadams
+# --------------------------------------- MAP BRUKER ----------------------------------------
+ssh cadams@10.152.135.57
 
 grep -vxFf mapped_done.txt all-pool-names.txt > map_queue.txt
 
@@ -18,9 +12,11 @@ do
     Rscript create_annotation_input.R "${array[0]}"
 done
 
-grep -vxFf done_done.txt done_pool.txt > filter-queue.txt
+# -------------------------------------- FILTER DATA ----------------------------------------
 
-name_file=filter-queue.txt
+grep -vxFf done_done.txt done_pool.txt > filter_queue.txt
+
+name_file=filter_queue.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
@@ -28,9 +24,6 @@ do
     Rscript filter_annotation_input.R "${array[0]}"
 done
 
-Rscript filter_annotation_input.R "TUM_lysn_25"
-
-filter_annotation_input.R "${array[0]}"
 name_file=pool-queue.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
@@ -47,57 +40,13 @@ do
     time Rscript filter_and_sum.R "${array[0]}"
 done
 
-scp Plots/prepare_ce_df.R cadams@10.152.135.57:/home/cadams
-# scp Plots/prepare_qc_in_trap.R cadams@10.152.135.57:/home/cadams
-# scp done_pool.txt cadams@10.152.135.57:/home/cadams
-
-# cp done_pool.txt ce_queue.txt
-grep -vxFf ce_done.txt done_pool.txt > ce-queue.txt
-
-name_file=ce_queue.txt
+name_file=all-pool-names.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    Rscript prepare_ce_df.R "${array[0]}"
+    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/master_spectrum_precursor.py "${array[0]}"
 done
-
-name_file=done_pool.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    Rscript prepare_ce_mz.R "${array[0]}"
-done
-
-name_file=done_pool.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    Rscript prepare_qc_in_trap.R "${array[0]}"
-done
-
-
-# cd /Users/adams/Projects/300K
-
-name_file=/Users/adams/Code/timsTOF-immunopeptide-prediction/done_pool.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 /Users/adams/Code/timsTOF-immunopeptide-prediction/Annotation/prosit_annotate.py "${array[0]}"
-done
-
-# /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 /Users/adams/Code/timsTOF-immunopeptide-prediction/Annotation/shuffle_split.py "test"
-
-# --------------------------------------- MAP BRUKER ----------------------------------------
-scp Annotation/bruker_map.R cadams@10.152.135.57:/home/cadams
-
-Rscript bruker_map.R "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/190926_TIMSiDE_LCMS02_sample-3_90min_R2_Slot1-30_01_3622.d" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/combined/txt" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/precursor-mapped/S3_R2.csv"
-
-
-
 
 # ---------------------------------------- ANNOTATE -----------------------------------------
 
@@ -129,36 +78,10 @@ do
 done
 
 # ---------------------------------------- CALIBRATE ----------------------------------------
+ssh cadams@10.152.135.57
 
 scp Annotation/frame_calibrate.py cadams@10.152.135.57:/home/cadams
 scp Annotation/sum_calibrate.py cadams@10.152.135.57:/home/cadams
-
-scp ann_queue.txt cadams@10.152.135.57:/home/cadams
-# scp Annotation/prosit_annotate_calibrate.py cadams@10.152.135.57:/home/cadams
-
-name_file=done_pool.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 prosit_annotate_calibrate.py "${array[0]}"
-    printf '%d %s' "$i" "${array[i]}"
-done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 calibrate_ce.py "${array[0]}"
-    printf '%d %s' "$i" "${array[i]}"
-done
-
-
-
-scp /Users/adams/Projects/300K/fundamentals/fundamentals/fragments.py cadams@10.152.135.57:/home/cadams/fundamentals/fundamentals
-
-grep -vxFf cal_done.txt  all-pool-names.txt > cal_queue.txt
 
 name_file=sample-pool-names.txt
 lines=`tail -n+1 $name_file`
@@ -177,51 +100,6 @@ do
     IFS=';' read -r -a array <<< "$line"
     time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 sum_calibrate.py "${array[0]}"
     printf '%d %s' "$i" "${array[i]}"
-done
-
-# --------------------------------------- SUM SPECTRA ---------------------------------------
-
-# scp cadams@10.152.135.57:/home/cadams/all-pool-names.txt .
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time Rscript Annotation/filter_and_sum.R "${array[0]}"
-done
-
-grep -vxFf un-annotated_done.txt all-pool-names.txt > annotation-queue.txt
-
-# name_file=all-pool-names.txt
-# lines=`tail -n+1 $name_file`
-# for line in $lines
-# do
-#     IFS=';' read -r -a array <<< "$line"
-#     time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/prosit_sum_annotate.py "${array[0]}"
-# done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/master_spectrum_precursor.py "${array[0]}"
-done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/annotate.py "${array[0]}"
-done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/filter_generate_hsf5.py "${array[0]}"
 done
 
 # --------------------------------------- GENERATE HDF5 ---------------------------------------
@@ -273,3 +151,44 @@ do
     IFS=';' read -r -a array <<< "$line"
     Rscript Plots/sa_boxplot_pairwise.R "${array[0]}"
 done
+
+# ----------------------------------------- CE PLOTS ----------------------------------------
+
+scp Plots/prepare_ce_df.R cadams@10.152.135.57:/home/cadams
+# scp Plots/prepare_qc_in_trap.R cadams@10.152.135.57:/home/cadams
+# scp done_pool.txt cadams@10.152.135.57:/home/cadams
+
+# cp done_pool.txt ce_queue.txt
+grep -vxFf ce_done.txt done_pool.txt > ce-queue.txt
+
+name_file=ce_queue.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    Rscript prepare_ce_df.R "${array[0]}"
+done
+
+name_file=done_pool.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    Rscript prepare_ce_mz.R "${array[0]}"
+done
+
+name_file=done_pool.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    Rscript prepare_qc_in_trap.R "${array[0]}"
+done
+
+# ------------------------------------ GENERAL BRUKER MAP -----------------------------------
+
+scp Annotation/bruker_map.R cadams@10.152.135.57:/home/cadams
+
+Rscript bruker_map.R "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/190926_TIMSiDE_LCMS02_sample-3_90min_R2_Slot1-30_01_3622.d" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/combined/txt" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/precursor-mapped/S3_R2.csv"
+
+
