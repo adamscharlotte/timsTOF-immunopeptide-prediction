b86a370d4fb8780234b7abf8421b37d24a323463
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-11 16:30:57 +0200

    Calculate the pairwise spectral angle at 20 ppm.

--- Annotation/calculate_pairwise_sa.py
+++ Annotation/calculate_pairwise_sa.py
@@ -33,63 +33,54 @@ pool = args.pool
 # pool = "TUM_aspn_36"
 
 base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
-unsummed_path = base_path + "full-truncated-qc/annotated-20ppm/" + pool + ".csv"
-sum_path = base_path + "precursor-consensus/annotated-20ppm/" + pool + ".csv"
+# unsummed_path = base_path + "full-truncated-qc/annotated-40-ppm/" + pool + ".csv"
+sum_path = base_path + "precursor-consensus/annotated-20-ppm/" + pool + ".csv"
 sa_path = base_path + "spectral-angle/"
 
-unsummed_df = pd.read_csv(unsummed_path)
+# unsummed_df = pd.read_csv(unsummed_path)
 sum_df = pd.read_csv(sum_path)
 
-unsummed_df.INTENSITIES = unsummed_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
-unsummed_df.MZ = unsummed_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
-unsummed_df.SEQUENCE_INT = unsummed_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
+# unsummed_df.INTENSITIES = unsummed_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
+# unsummed_df.MZ = unsummed_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
+# unsummed_df.SEQUENCE_INT = unsummed_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
 
 sum_df.INTENSITIES = sum_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
 sum_df.MZ = sum_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
 sum_df.SEQUENCE_INT = sum_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
 
-# ----------------------------------------- UN-SUMMED -------------------------------------------
+# # ----------------------------------------- UN-SUMMED -------------------------------------------
 
-col_filter = ['SCORE']
-unsummed_df[col_filter] = unsummed_df[unsummed_df[col_filter] >= 70][col_filter]
-filtered_unsummed_df = unsummed_df.dropna()
+# col_filter = ['SCORE']
+# unsummed_df[col_filter] = unsummed_df[unsummed_df[col_filter] >= 70][col_filter]
+# filtered_unsummed_df = unsummed_df.dropna()
 
-filtered_unsummed_df['PRECURSOR_CHARGE'].value_counts()
-filtered_unsummed_df['COLLISION_ENERGY'].min()
-# filtered_unsummed_df['aligned_collision_energy'].min()
-filtered_unsummed_df.columns
-grouped_charge_df = filtered_unsummed_df.groupby('PRECURSOR_CHARGE')
+# filtered_unsummed_df['PRECURSOR_CHARGE'].value_counts()
+# filtered_unsummed_df['COLLISION_ENERGY'].min()
+# filtered_unsummed_df.columns
+# grouped_charge_df = filtered_unsummed_df.groupby('PRECURSOR_CHARGE')
 
-# filtered_unsummed_df[['PRECURSOR', 'aligned_collision_energy', 'COLLISION_ENERGY']].sort_values(by = ['aligned_collision_energy'])
-
-for charge, df_charge in grouped_charge_df:
-    # random_10_precursors = df_charge['PRECURSOR'].drop_duplicates().sample(n = 10, random_state = 43)
-    # df_random_10 = df_charge[df_charge['PRECURSOR'].isin(random_10_precursors)]
-    # grouped_df_random_10 = df_random_10.groupby('PRECURSOR')
-    grouped_df_random_10 = df_charge.groupby('PRECURSOR')
-    df_random_10_pairs = pd.DataFrame({'combination' : [], 'A' : '', 'B': '', 'INTENSITIES_A' : [], 'INTENSITIES_B' : [], 'PRECURSOR': ''})
-    for group, df_group in grouped_df_random_10:
-        if len(df_group) == 1:
-            continue
-        else:
-            comb_list = list(combinations(df_group.FRAME, 2))
-            combdf = pd.DataFrame({'combination':comb_list})
-            combdf['combination'] = combdf['combination'].astype(str)
-            combdf.combination = combdf.combination.str.strip(")(").str.split(", ").apply(lambda s: [int(x) for x in s])
-            combdf[['A','B']] = pd.DataFrame(combdf.combination.tolist(), index= combdf.index)
-            # mergdfA = pd.merge(combdf,df_random_10[['FRAME','INTENSITIES']],left_on='A', right_on='FRAME', how='left')
-            mergdfA = pd.merge(combdf,df_group[['FRAME','INTENSITIES']],left_on='A', right_on='FRAME', how='left')
-            mergdfA = mergdfA.drop(columns=['FRAME'])
-            mergdfA = mergdfA.rename(columns={'INTENSITIES': 'INTENSITIES_A'})
-            # mergdfAB = pd.merge(mergdfA,df_random_10[['FRAME','INTENSITIES', 'PRECURSOR']],left_on='B', right_on='FRAME', how='left')
-            mergdfAB = pd.merge(mergdfA,df_group[['FRAME','INTENSITIES', 'PRECURSOR']],left_on='B', right_on='FRAME', how='left')
-            mergdfAB = mergdfAB.drop(columns=['FRAME'])
-            mergdfAB = mergdfAB.rename(columns={'INTENSITIES': 'INTENSITIES_B'})
-            df_random_10_pairs = pd.concat([df_random_10_pairs, mergdfAB], ignore_index=True)
-    df_random_10_pairs["SPECTRAL_ANGLE"] = df_random_10_pairs[['INTENSITIES_A','INTENSITIES_B']].apply(lambda x : get_spectral_angle(x), axis=1)
-    df_random_10_pairs["SPECTRAL_ANGLE"].fillna(0, inplace=True)
-    # df_random_10_pairs.to_csv(sa_path + "pairwise/frames/20-ppm/" + pool + '_' + str(charge) + '.csv')
-    df_random_10_pairs.to_csv(sa_path + "pairwise/frames/all/" + pool + '_' + str(charge) + '.csv')
+# for charge, df_charge in grouped_charge_df:
+#     df_grouped = df_charge.groupby('PRECURSOR')
+#     df_pairs = pd.DataFrame({'combination' : [], 'A' : '', 'B': '', 'INTENSITIES_A' : [], 'INTENSITIES_B' : [], 'PRECURSOR': ''})
+#     for group, df_group in df_grouped:
+#         if len(df_group) == 1:
+#             continue
+#         else:
+#             comb_list = list(combinations(df_group.FRAME, 2))
+#             combdf = pd.DataFrame({'combination':comb_list})
+#             combdf['combination'] = combdf['combination'].astype(str)
+#             combdf.combination = combdf.combination.str.strip(")(").str.split(", ").apply(lambda s: [int(x) for x in s])
+#             combdf[['A','B']] = pd.DataFrame(combdf.combination.tolist(), index= combdf.index)
+#             mergdfA = pd.merge(combdf,df_group[['FRAME','INTENSITIES']],left_on='A', right_on='FRAME', how='left')
+#             mergdfA = mergdfA.drop(columns=['FRAME'])
+#             mergdfA = mergdfA.rename(columns={'INTENSITIES': 'INTENSITIES_A'})
+#             mergdfAB = pd.merge(mergdfA,df_group[['FRAME','INTENSITIES', 'PRECURSOR']],left_on='B', right_on='FRAME', how='left')
+#             mergdfAB = mergdfAB.drop(columns=['FRAME'])
+#             mergdfAB = mergdfAB.rename(columns={'INTENSITIES': 'INTENSITIES_B'})
+#             df_pairs = pd.concat([df_pairs, mergdfAB], ignore_index=True)
+#     df_pairs["SPECTRAL_ANGLE"] = df_pairs[['INTENSITIES_A','INTENSITIES_B']].apply(lambda x : get_spectral_angle(x), axis=1)
+#     df_pairs["SPECTRAL_ANGLE"].fillna(0, inplace=True)
+#     df_pairs.to_csv(sa_path + "pairwise/frames/all-40-ppm/" + pool + '_' + str(charge) + '.csv')
 
 # ------------------------------------------- SUMMED --------------------------------------------
 
@@ -102,17 +93,13 @@ filtered_sum_df.columns
 filtered_sum_df['PRECURSOR_CHARGE'].value_counts()
 filtered_sum_df['SCORE'].unique()
 filtered_sum_df['COLLISION_ENERGY'].min()
-# filtered_sum_df['aligned_collision_energy'].min()
 
 grouped_charge_df = filtered_sum_df.groupby('PRECURSOR_CHARGE')
 
 for charge, df_charge in grouped_charge_df:
-    # random_10_precursors = df_charge['SEQUENCE'].drop_duplicates().sample(n = 10, random_state = 43)
-    # df_random_10 = df_charge[df_charge['SEQUENCE'].isin(random_10_precursors)]
-    # grouped_df_random_10 = df_random_10.groupby('SEQUENCE')
-    grouped_df_random_10 = df_charge.groupby('SEQUENCE')
-    df_random_10_pairs = pd.DataFrame({'combination' : [], 'A' : '', 'B': '', 'INTENSITIES_A' : [], 'INTENSITIES_B' : [], 'SEQUENCE': ''})
-    for group, df_group in grouped_df_random_10:
+    df_grouped = df_charge.groupby('SEQUENCE')
+    df_pairs = pd.DataFrame({'combination' : [], 'A' : '', 'B': '', 'INTENSITIES_A' : [], 'INTENSITIES_B' : [], 'SEQUENCE': ''})
+    for group, df_group in df_grouped:
         if len(df_group) == 1:
             continue
         else:
@@ -121,15 +108,13 @@ for charge, df_charge in grouped_charge_df:
             combdf['combination'] = combdf['combination'].astype(str)
             combdf.combination = combdf.combination.str.strip(")(").str.split(", ").apply(lambda s: [int(x) for x in s])
             combdf[['A','B']] = pd.DataFrame(combdf.combination.tolist(), index= combdf.index)
-            # mergdfA = pd.merge(combdf,df_random_10[['PRECURSOR','INTENSITIES']],left_on='A', right_on='PRECURSOR', how='left')
             mergdfA = pd.merge(combdf,df_charge[['PRECURSOR','INTENSITIES']],left_on='A', right_on='PRECURSOR', how='left')
             mergdfA = mergdfA.drop(columns=['PRECURSOR'])
             mergdfA = mergdfA.rename(columns={'INTENSITIES': 'INTENSITIES_A'})
-            # mergdfAB = pd.merge(mergdfA,df_random_10[['PRECURSOR','INTENSITIES', 'SEQUENCE']],left_on='B', right_on='PRECURSOR', how='left')
             mergdfAB = pd.merge(mergdfA,df_charge[['PRECURSOR','INTENSITIES', 'SEQUENCE']],left_on='B', right_on='PRECURSOR', how='left')
             mergdfAB = mergdfAB.drop(columns=['PRECURSOR'])
             mergdfAB = mergdfAB.rename(columns={'INTENSITIES': 'INTENSITIES_B'})
-            df_random_10_pairs = pd.concat([df_random_10_pairs, mergdfAB], ignore_index=True)
-    df_random_10_pairs["SPECTRAL_ANGLE"] = df_random_10_pairs[['INTENSITIES_A','INTENSITIES_B']].apply(lambda x : get_spectral_angle(x), axis=1)
-    df_random_10_pairs["SPECTRAL_ANGLE"].fillna(0, inplace=True)
-    df_random_10_pairs.to_csv(sa_path + "pairwise/precursors/all/" + pool + '_' + str(charge) + '.csv')
+            df_pairs = pd.concat([df_pairs, mergdfAB], ignore_index=True)
+    df_pairs["SPECTRAL_ANGLE"] = df_pairs[['INTENSITIES_A','INTENSITIES_B']].apply(lambda x : get_spectral_angle(x), axis=1)
+    df_pairs["SPECTRAL_ANGLE"].fillna(0, inplace=True)
+    df_pairs.to_csv(sa_path + "pairwise/precursors/all-20-ppm/" + pool + '_' + str(charge) + '.csv')
