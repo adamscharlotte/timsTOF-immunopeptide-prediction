8e5ae82b831cd79ae8ebd28e3936971203a5dae3
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-10-11 16:27:42 +0200

    Plot loss and loss_val.

--- Plots/loss_plot.R
+++ Plots/loss_plot.R
@@ -2,16 +2,21 @@ library(tidyverse)
 library(data.table)
 library(ggplot2)
 
-file_name <- "log_precursor_20ppm_calibrated_refinement"
-plot_title <- "Precursor - Calibrated CE - Refinement"
+file_name <- "log_precursor_20ppm_calibrated_scratch"
+plot_title <- "Precursor - Calibrated CE - Scratch"                      #  Scratch Refinement # nolint
 log_base <- "/Users/adams/Projects/300K/2022-library-run/Annotation/log/"
 log_path <- paste(log_base, file_name, ".csv", sep = "")
 
 tbl_log <- fread(log_path) %>% as_tibble()
 
-plot_log <- ggplot(tbl_log, aes(x = epoch, y = loss)) +
-    geom_line() +
-    labs(title = plot_title)
+plot_log <- ggplot(tbl_log, aes(x = epoch)) +
+    geom_line(aes(y = loss, colour = "loss")) +
+    geom_line(aes(y = val_loss, colour = "val_loss")) +
+    labs(title = plot_title) +
+    theme_minimal() +
+    scale_color_manual(values = c("#a2bce0", "#f33b16")) +
+    labs(color = "")
+plot_log
 
 plot_base <- "/Users/adams/Projects/300K/Results/Figures/Log/"
 plot_path <- paste(plot_base, file_name, ".pdf", sep = "")
