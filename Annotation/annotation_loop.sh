086c4ec3d3f40c6b45f94bfeaa36d0b5df8c4903
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-11-03 11:42:39 +0100

    Clean up loop bash code

--- Annotation/annotation_loop.sh
+++ Annotation/annotation_loop.sh
@@ -3,6 +3,7 @@
 ssh cadams@10.152.135.57
 
 grep -vxFf mapped_done.txt all-pool-names.txt > map_queue.txt
+grep -vxFf cali_done.txt all-pool-names.txt > annot_queue.txt
 
 name_file=map_queue.txt
 lines=`tail -n+1 $name_file`
@@ -14,71 +15,41 @@ done
 
 # -------------------------------------- FILTER DATA ----------------------------------------
 
-grep -vxFf done_done.txt done_pool.txt > filter_queue.txt
-
-name_file=filter_queue.txt
+name_file=all-pool-names.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    Rscript filter_annotation_input.R "${array[0]}"
+    # Rscript filter_annotation_input.R "${array[0]}"
+    # time Rscript filter_and_add_ce.R "${array[0]}" "${array[1]}"
+    # time Rscript filter_and_sum.R "${array[0]}"
+    time Rscript Annotation/scan_filter_sum.R "${array[0]}"
 done
 
-name_file=pool-queue.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time Rscript filter_and_add_ce.R "${array[0]}" "${array[1]}"
-done
+# -------------------------------- CREATE CONSENSUS SPECTRA ---------------------------------
 
 name_file=all-pool-names.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    time Rscript filter_and_sum.R "${array[0]}"
-done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/master_spectrum_precursor.py "${array[0]}"
+    # time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/master_spectrum_precursor.py "${array[0]}"
+    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/scan_master_spectrum.py "${array[0]}"
 done
 
 # ---------------------------------------- ANNOTATE -----------------------------------------
 
 name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/prosit_annotate.py "${array[0]}"
-done
-
-grep -vxFf ann_done.txt all-pool-names.txt > ann_queue.txt
-
-name_file=sample-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/frame_annotate.py "${array[0]}"
-done
-
-
-grep -vxFf annot_done.txt all-pool-names.txt > annot_queue.txt
-
-# name_file=all-pool-names.txt
 # name_file=sample-pool-names.txt
-name_file=annot_queue
+# name_file=annot_queue
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/sum_annotate.py "${array[0]}"
+    # time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/prosit_annotate.py "${array[0]}"
+    # time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/frame_annotate.py "${array[0]}"
+    # time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/sum_annotate.py "${array[0]}"
+    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/scan_annotate.py "${array[0]}"
 done
 
 /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/sum_annotate.py "TUM_HLA2_154"
@@ -87,17 +58,7 @@ done
 ssh cadams@10.152.135.57
 
 scp Annotation/frame_calibrate.py cadams@10.152.135.57:/home/cadams
-scp Annotation/sum_calibrate.py cadams@10.152.135.57:/home/cadams
 scp annot_queue.txt cadams@10.152.135.57:/home/cadams
-
-name_file=sample-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 frame_calibrate.py "${array[0]}"
-    printf '%d %s' "$i" "${array[i]}"
-done
 
 # name_file=all-pool-names.txt
 name_file=annot_queue.txt
@@ -106,8 +67,20 @@ lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 sum_calibrate.py "${array[0]}"
+    # time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 frame_calibrate.py "${array[0]}"
+    # time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 sum_calibrate.py "${array[0]}"
+    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 scan_calibrate.py "${array[0]}"
     printf '%d %s' "$i" "${array[i]}"
+done
+
+# --------------------------------- MAP FULL-LENGTH SEQUENCES --------------------------------
+
+name_file=all-pool-names.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    time Rscript Annotation/full_length_mapper.R "${array[0]}"
 done
 
 # --------------------------------------- GENERATE HDF5 ---------------------------------------
@@ -117,7 +90,8 @@ lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/calibrated_hdf5.py "${array[0]}"
+    # time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/calibrated_hdf5.py "${array[0]}"
+    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/scan_hdf5.py "${array[0]}"
 done
 
 # --------------------------------------- SHUFFLE SPLIT ----------------------------------------
@@ -215,57 +189,3 @@ done
 scp Annotation/bruker_map.R cadams@10.152.135.57:/home/cadams
 
 Rscript bruker_map.R "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/190926_TIMSiDE_LCMS02_sample-3_90min_R2_Slot1-30_01_3622.d" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/combined/txt" "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/PXD030334/precursor-mapped/S3_R2.csv"
-
-# ----------------------------------- SCAN-LEVEL CONSENSUS ----------------------------------
-
-name_file=all-pool-names.txt
-# name_file=sample-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time Rscript Annotation/scan_filter_sum.R "${array[0]}"
-done
-
-name_file=all-pool-names.txt
-# name_file=sample-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/scan_master_spectrum.py "${array[0]}"
-done
-
-name_file=all-pool-names.txt
-# name_file=sample-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/scan_annotate.py "${array[0]}"
-done
-
-
-scp cali_done.txt cadams@10.152.135.57:/home/cadams
-scp Annotation/scan_calibrate.py cadams@10.152.135.57:/home/cadams
-ssh cadams@10.152.135.57
-
-grep -vxFf cali_done.txt all-pool-names.txt > cali_queue.txt
-
-# name_file=all-pool-names.txt
-name_file=cali_queue.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /home/cadams/anaconda3/envs/prosit-annotate/bin/python3 scan_calibrate.py "${array[0]}"
-    printf '%d %s' "$i" "${array[i]}"
-done
-
-name_file=all-pool-names.txt
-lines=`tail -n+1 $name_file`
-for line in $lines
-do
-    IFS=';' read -r -a array <<< "$line"
-    time /Users/adams/opt/miniconda3/envs/prosit-annotate/bin/python3 Annotation/calibrated_hdf5.py "${array[0]}"
-done
