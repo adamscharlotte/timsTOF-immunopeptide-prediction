aa6eacfccf5dc72e7d75012da184b9134709d900
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-12-01 09:01:45 +0100

    Calculate the pairwise SA at scan level.

--- Annotation/calculate_spectral_angle.py
+++ Annotation/calculate_spectral_angle.py
@@ -52,10 +52,12 @@ base_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeToo
 
 calibrated_unsummed_path = base_path + "full-truncated-qc/calibrated-20ppm/" + pool + ".csv"
 calibrated_sum_path = base_path + "precursor-consensus/calibrated-20ppm/" + pool + ".csv"
+calibrated_scan_path = base_path + "precursor-consensus/calibrated-20ppm/" + pool + ".csv"
 sa_path = base_path + "spectral-angle/"
 
 unsummed_df = pd.read_csv(calibrated_unsummed_path)
 sum_df = pd.read_csv(calibrated_sum_path)
+scan_df = pd.read_csv(calibrated_scan_path)
 
 unsummed_df.INTENSITIES = unsummed_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
 unsummed_df.MZ = unsummed_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
@@ -65,7 +67,11 @@ sum_df.INTENSITIES = sum_df.INTENSITIES.str.split(";").apply(lambda s: [float(x)
 sum_df.MZ = sum_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
 sum_df.SEQUENCE_INT = sum_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
 
-# ----------------------------------------- UN-SUMMED -------------------------------------------
+scan_df.INTENSITIES = scan_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
+scan_df.MZ = scan_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
+scan_df.SEQUENCE_INT = scan_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
+
+# ------------------------------------------- FRAME ---------------------------------------------
 
 col_filter = ['SCORE']
 unsummed_df[col_filter] = unsummed_df[unsummed_df[col_filter] >= 70][col_filter]
@@ -104,7 +110,7 @@ for charge, df_charge in grouped_charge_df:
     df_random_10["SPECTRAL_ANGLE_CAL"].fillna(0, inplace=True)
     df_random_10.to_csv(sa_path + "frames/20-ppm/" + pool + '_' + str(charge) + '.csv')
 
-# ------------------------------------------- SUMMED --------------------------------------------
+# ------------------------------------------ PRECURSOR ------------------------------------------
 
 col_filter = ['SCORE']
 sum_df[col_filter] = sum_df[sum_df[col_filter] >= 70][col_filter]
@@ -142,3 +148,43 @@ for charge, df_charge in grouped_charge_df:
     df_random_10["SPECTRAL_ANGLE_CAL"] = df_random_10[['INTENSITIES','PREDICTED_INTENSITY_CAL']].apply(lambda x : get_spectral_angle(x), axis=1)
     df_random_10["SPECTRAL_ANGLE_CAL"].fillna(0, inplace=True)
     df_random_10.to_csv(sa_path + "precursors/20-ppm/" + pool + '_' + str(charge) + '.csv')
+
+# -------------------------------------------- SCAN ---------------------------------------------
+
+col_filter = ['SCORE']
+scan_df[col_filter] = scan_df[scan_df[col_filter] >= 70][col_filter]
+filtered_scan_df = scan_df.dropna()
+
+filtered_scan_df.columns
+
+filtered_scan_df['PRECURSOR_CHARGE'].value_counts()
+filtered_scan_df['SCORE'].unique()
+filtered_scan_df['ORIG_COLLISION_ENERGY'].min()
+filtered_scan_df['aligned_collision_energy'].min()
+
+grouped_charge_df = filtered_scan_df.groupby('PRECURSOR_CHARGE')
+
+predictor = PROSITpredictor(server="10.152.135.57:8500")
+
+for charge, df_charge in grouped_charge_df:
+    random_10_precursors = df_charge['SEQUENCE'].drop_duplicates().sample(n = 10, random_state = 43)
+    df_random_10 = df_charge[df_charge['SEQUENCE'].isin(random_10_precursors)]
+    df_random_10['SEQUENCE'].drop_duplicates()
+    predictions = predictor.predict(sequences=df_random_10['MODIFIED_SEQUENCE'].values.tolist(),
+                                    charges=df_random_10["PRECURSOR_CHARGE"].values.tolist(),
+                                    collision_energies=df_random_10["ORIG_COLLISION_ENERGY"].values/100.0,
+                                                        models=['Prosit_2020_intensity_hcd'],
+                                                        disable_progress_bar=True)
+    df_random_10['PREDICTED_INTENSITY'] = predictions['Prosit_2020_intensity_hcd']['intensity'].tolist()
+    df_random_10["SPECTRAL_ANGLE"] = df_random_10[['INTENSITIES','PREDICTED_INTENSITY']].apply(lambda x : get_spectral_angle(x), axis=1)
+    df_random_10["SPECTRAL_ANGLE"].fillna(0, inplace=True)
+    predictions_cal = predictor.predict(sequences=df_random_10['MODIFIED_SEQUENCE'].values.tolist(),
+                                    charges=df_random_10["PRECURSOR_CHARGE"].values.tolist(),
+                                    collision_energies=df_random_10["aligned_collision_energy"].values/100.0,
+                                                        models=['Prosit_2020_intensity_hcd'],
+                                                        disable_progress_bar=True)
+    df_random_10['PREDICTED_INTENSITY_CAL'] = predictions_cal['Prosit_2020_intensity_hcd']['intensity'].tolist()
+    df_random_10["SPECTRAL_ANGLE_CAL"] = df_random_10[['INTENSITIES','PREDICTED_INTENSITY_CAL']].apply(lambda x : get_spectral_angle(x), axis=1)
+    df_random_10["SPECTRAL_ANGLE_CAL"].fillna(0, inplace=True)
+    df_random_10.to_csv(sa_path + "scans/20-ppm/" + pool + '_' + str(charge) + '.csv')
+
