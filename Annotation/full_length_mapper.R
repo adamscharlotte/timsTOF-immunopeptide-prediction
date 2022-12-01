8d4b138a07012f13bfbafb335cb4797a5b0b76ea
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-12-01 09:59:38 +0100

    Create a df to an observed peptide to its full lenght.

--- Annotation/full_length_mapper.R
+++ Annotation/full_length_mapper.R
@@ -5,14 +5,14 @@ library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
 pool <- args[1]
-# pool <- "TUM_HLA_16"
+# pool <- "TUM_lysn_33"
 
 # base_path <- "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/" # nolint
 base_path <- "/Users/adams/Projects/300K/2022-library-run/"
 meta_path <- paste(base_path, "Metadata/full-pool-sequence.txt", sep = "")
 meta_qc_path <- paste(base_path, "Metadata/qc-peptides.txt", sep = "")
-txt_output_path <- paste(base_path, "Metadata/info-sum.txt", sep = "")
 mapped_precursor_path <- paste(base_path, "Annotation/precursor-mapped/", pool, ".csv", sep = "")  # nolint
+txt_output_path <- paste(base_path, "Metadata/identification-summary.txt", sep = "") # nolint
 
 spaceless <- function(x) {
     colnames(x) <- gsub(" ", "_", colnames(x))
@@ -24,75 +24,68 @@ tbl_obs_sequence <- fread(mapped_precursor_path) %>%
     dplyr::rename(obs_sequence = Sequence) %>%
     rename_with(toupper) %>%
     spaceless() %>%
-    select(OBS_SEQUENCE) %>%
+    select(OBS_SEQUENCE, PROTEINS) %>%
     distinct()
 
 tbl_pool_all <- fread(meta_path) %>%
     as_tibble() %>%
-    # filter(pool_name == pool) %>%
+    filter(pool_name == pool) %>%
     mutate(trunc_sequence = str_sub(Sequence, start = -7)) %>%
+    dplyr::rename(PROTEINS = pool_name) %>%
     rename_with(toupper) %>%
-    select(POOL_NAME, SEQUENCE, TRUNC_SEQUENCE) %>%
+    select(SEQUENCE, TRUNC_SEQUENCE, PROTEINS) %>%
     distinct()
 
+# Remove all sequences that have the same truncated sequence
 tbl_pool_trunc <- tbl_pool_all %>%
-    add_count(POOL_NAME, TRUNC_SEQUENCE) %>%
+    add_count(TRUNC_SEQUENCE) %>%
     filter(n == 1) %>%
     select(-n)
 
+tbl_pool_all %>%
+    add_count(TRUNC_SEQUENCE) %>%
+    filter(!n == 1) %>%
+    select(-n) %>%
+    arrange(TRUNC_SEQUENCE)
+
+# Use the full length sequence as the truncated
 tbl_pool_full <- tbl_pool_all %>%
-    add_count(POOL_NAME, TRUNC_SEQUENCE) %>%
+    add_count(TRUNC_SEQUENCE) %>%
     filter(n > 1) %>%
     select(-c(n, TRUNC_SEQUENCE)) %>%
-    mutate(TRUNC_SEQUENCE = POOL_NAME)
+    mutate(TRUNC_SEQUENCE = SEQUENCE) %>%
+    distinct()
 
 tbl_pool_peptides <- rbind(tbl_pool_trunc, tbl_pool_full)
 
 tbl_qc_peptides <- fread(meta_qc_path) %>%
     as_tibble() %>%
     rename_with(toupper) %>%
-    select(SEQUENCE, TRUNC_SEQUENCE)
+    select(SEQUENCE, TRUNC_SEQUENCE, PROTEINS)
 
 tbl_peptides <- rbind(tbl_pool_peptides, tbl_qc_peptides)
-
-
-tbl_peptides %>%
-    count(TRUNC_SEQUENCE) %>%
-    arrange((desc(n)))
 
 tbl_mapped_sequences <- tbl_obs_sequence %>%
     merge(tbl_peptides) %>%
     as_tibble()
 
-tbl_filtered_sequences <- tbl_mapped_sequences %>%
-    filter(stringr::str_ends(OBS_SEQUENCE, TRUNC_SEQUENCE))
+tbl_sequences_full <- tbl_mapped_sequences %>%
+    filter(OBS_SEQUENCE == SEQUENCE) %>%
+    select(OBS_SEQUENCE, SEQUENCE, PROTEINS)
+
+tbl_sequences_trunc <- tbl_mapped_sequences %>%
+    filter(!OBS_SEQUENCE %in% tbl_sequences_full$OBS_SEQUENCE) %>%
+    filter(stringr::str_ends(OBS_SEQUENCE, TRUNC_SEQUENCE)) %>%
+    select(OBS_SEQUENCE, SEQUENCE, PROTEINS)
+
+tbl_sequences_trunc %>% distinct() %>% count(PROTEINS)
+
+tbl_filtered_sequences <- rbind(tbl_sequences_full, tbl_sequences_trunc)
 
 tbl_filtered_sequences %>%
-    filter(!OBS_SEQUENCE == SEQUENCE) %>%
-    print(n = 150)
+    add_count(OBS_SEQUENCE) %>%
+    filter(n > 1) %>%
+    arrange(OBS_SEQUENCE)
 
-tbl_filtered_sequences %>%
-    count(OBS_SEQUENCE) %>%
-    arrange((desc(n)))
-
-tbl_filtered_sequences %>%
-    filter(OBS_SEQUENCE == c("YELTQPPSV", "YVLTQPPSV")) %>%
-    distinct()
-
-tbl_annotation <- tbl_mapped_precursor %>%
-    filter(obs_sequence %in% tbl_filtered_precursors$obs_sequence &
-        Precursor %in% tbl_filtered_precursors$Precursor) %>%
-    filter(Score >= 70) %>%
-    rename_with(toupper) %>%
-    group_by(PRECURSOR) %>%
-    mutate(combined_INTENSITIES = paste0(INTENSITIES, collapse = ";")) %>%
-    mutate(combined_MZ = paste0(MZ, collapse = ";")) %>%
-    mutate(RETENTION_TIME = median(RETENTION_TIME)) %>%
-    ungroup() %>%
-    select(RAW_FILE, SCAN_NUMBER, MODIFIED_SEQUENCE, CHARGE, FRAGMENTATION,
-    MASS_ANALYZER, SCAN_EVENT_NUMBER, MASS, SCORE, REVERSE, RETENTION_TIME,
-    combined_MZ, combined_INTENSITIES, COLLISION_ENERGY, PRECURSOR) %>%
-    distinct()
-
-output_path <- paste(base_path, "Annotation/precursor-consensus/un-annotated/", pool, ".csv", sep = "") # nolint
-fwrite(tbl_annotation, output_path)
+output_path <- paste(base_path, "Annotation/full-length-map/", pool, ".csv", sep = "") # nolint
+fwrite(tbl_filtered_sequences, output_path)
