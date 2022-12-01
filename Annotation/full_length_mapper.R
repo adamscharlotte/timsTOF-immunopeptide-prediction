48cc487346ce8fd3044c50576bdf9863de1edf01
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-12-01 10:53:41 +0100

    Simplify code, now no truncated peptides are missed.

--- Annotation/full_length_mapper.R
+++ Annotation/full_length_mapper.R
@@ -30,62 +30,25 @@ tbl_obs_sequence <- fread(mapped_precursor_path) %>%
 tbl_pool_all <- fread(meta_path) %>%
     as_tibble() %>%
     filter(pool_name == pool) %>%
-    mutate(trunc_sequence = str_sub(Sequence, start = -7)) %>%
     dplyr::rename(PROTEINS = pool_name) %>%
     rename_with(toupper) %>%
-    select(SEQUENCE, TRUNC_SEQUENCE, PROTEINS) %>%
+    select(SEQUENCE, PROTEINS) %>%
     distinct()
-
-# Remove all sequences that have the same truncated sequence
-tbl_pool_trunc <- tbl_pool_all %>%
-    add_count(TRUNC_SEQUENCE) %>%
-    filter(n == 1) %>%
-    select(-n)
-
-tbl_pool_all %>%
-    add_count(TRUNC_SEQUENCE) %>%
-    filter(!n == 1) %>%
-    select(-n) %>%
-    arrange(TRUNC_SEQUENCE)
-
-# Use the full length sequence as the truncated
-tbl_pool_full <- tbl_pool_all %>%
-    add_count(TRUNC_SEQUENCE) %>%
-    filter(n > 1) %>%
-    select(-c(n, TRUNC_SEQUENCE)) %>%
-    mutate(TRUNC_SEQUENCE = SEQUENCE) %>%
-    distinct()
-
-tbl_pool_peptides <- rbind(tbl_pool_trunc, tbl_pool_full)
 
 tbl_qc_peptides <- fread(meta_qc_path) %>%
     as_tibble() %>%
     rename_with(toupper) %>%
-    select(SEQUENCE, TRUNC_SEQUENCE, PROTEINS)
+    select(SEQUENCE, PROTEINS)
 
-tbl_peptides <- rbind(tbl_pool_peptides, tbl_qc_peptides)
+tbl_peptides <- rbind(tbl_pool_all, tbl_qc_peptides)
 
 tbl_mapped_sequences <- tbl_obs_sequence %>%
     merge(tbl_peptides) %>%
     as_tibble()
 
-tbl_sequences_full <- tbl_mapped_sequences %>%
-    filter(OBS_SEQUENCE == SEQUENCE) %>%
-    select(OBS_SEQUENCE, SEQUENCE, PROTEINS)
-
-tbl_sequences_trunc <- tbl_mapped_sequences %>%
-    filter(!OBS_SEQUENCE %in% tbl_sequences_full$OBS_SEQUENCE) %>%
-    filter(stringr::str_ends(OBS_SEQUENCE, TRUNC_SEQUENCE)) %>%
-    select(OBS_SEQUENCE, SEQUENCE, PROTEINS)
-
-tbl_sequences_trunc %>% distinct() %>% count(PROTEINS)
-
-tbl_filtered_sequences <- rbind(tbl_sequences_full, tbl_sequences_trunc)
-
-tbl_filtered_sequences %>%
-    add_count(OBS_SEQUENCE) %>%
-    filter(n > 1) %>%
-    arrange(OBS_SEQUENCE)
+tbl_filtered_sequences <- tbl_mapped_sequences %>%
+    filter(endsWith(SEQUENCE, OBS_SEQUENCE)) %>%
+    filter(str_length(OBS_SEQUENCE) <= str_length(SEQUENCE))
 
 output_path <- paste(base_path, "Annotation/full-length-map/", pool, ".csv", sep = "") # nolint
 fwrite(tbl_filtered_sequences, output_path)
