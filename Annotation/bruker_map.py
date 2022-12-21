c1477fa3ce496a542a499af1f3e986a215a8db1c
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-12-21 16:07:27 +0100

    Extract the min and max frame for each pool.

---
+++ Annotation/bruker_map.py
@@ -0,0 +1,23 @@
+# /home/cadams/anaconda3/envs/prosit-annotate/bin/python3
+
+import sqlite3
+import pandas as pd
+import argparse, pathlib
+parser = argparse.ArgumentParser()
+parser.add_argument("pool", type=str)
+args = parser.parse_args()
+pool = args.pool
+
+raw_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/Analysis/" + pool + "_01_01/TIMS-30min-R1-TIMS_semitryptic/230719_f1-" + pool + "_01_01-TIMS-30min-R1.d"
+tdf_path = raw_path + "/analysis.tdf"
+con = sqlite3.connect(tdf_path)
+cur = con.cursor()
+pasef_df = pd.read_sql_query("SELECT * from PasefFrameMsMsInfo", con)
+con.close()
+
+min_frame = pasef_df.Frame.min()
+max_frame = pasef_df.Frame.max()
+
+f = open("/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/tryptic-frames.csv", "a")
+f.write(f"{pool}, {min_frame}, {max_frame}" + '\n')
+f.close()
