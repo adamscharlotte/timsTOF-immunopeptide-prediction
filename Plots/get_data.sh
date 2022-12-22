0d7d0db8b061db3bcb851d4090e0512116627c36
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2022-12-22 14:46:20 +0100

    Get the tryptic accumulatedMsmsScans files

--- Plots/get_data.sh
+++ Plots/get_data.sh
@@ -1,16 +1,16 @@
 ssh cadams@10.152.135.57
 
 scp cadams@10.152.135.57:/home/cadams
-cd /media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Searches
+cd /media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/TUM-Searches
 
 mkdir accumulatedMsmsScans
 
-name_file=pool_plate.txt
+name_file=pool_names.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=',' read -r -a array <<< "$line"
-    cp ${array[1]}/${array[0]}/combined/txt/accumulatedMsmsScans.txt accumulatedMsmsScans/"${array[0]}".txt
+    cp ${array[0]}/combined/txt/accumulatedMsmsScans.txt accumulatedMsmsScans/"${array[0]}".txt
 done
 
 scp cadams@10.152.135.57:/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Searches/accumulatedMsmsScans/*.txt .
