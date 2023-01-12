38e8d1d4fd0ce36877d82d7d7a15c8ffe66dc804
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-01-12 11:02:52 +0100

    Adjust the CE plot input.

--- Plots/prepare_ce_df.R
+++ Plots/prepare_ce_df.R
@@ -2,29 +2,34 @@
 
 library(tidyverse)
 library(data.table)
-library(timsr)
 
 args <- commandArgs(trailingOnly = TRUE)
 pool <- args[1]
-# pool <- "TUM_aspn_1"
+# pool <- "TUM_aspn_8"
 
-base_path <- "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/" # nolint
-meta_path <- paste(base_path, "Metadata/full-pool-sequence.txt", sep = "")
-mapped_precursor_path <- paste(base_path, "Annotation/precursor-mapped/", pool, ".csv", sep = "")
-# mapped_precursor_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/precursor-mapped/TUM_aspn_1.csv"
-ce_output_path <- paste(base_path, "Annotation/QC-collission-energy.csv", sep = "")
-# ce_output_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/QC-collission-energy.csv"
+base_path <- "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/" # nolint
+un_annotated_ce_path <- paste(base_path, "full-truncated-qc/un-annotated-ce/", pool, ".csv", sep = "") # nolint
+mapped_precursor_path <- paste(base_path, "precursor-mapped/", pool, ".csv", sep = "") # nolint
+# mapped_precursor_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/precursor-mapped/TUM_aspn_1.csv" # nolint
+ce_output_path <- paste(base_path, "QC-collission-energies.csv", sep = "")
+# ce_output_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/QC-collission-energy.csv" # nolint
 
 spaceless <- function(x) {
     colnames(x) <- gsub(" ", "_", colnames(x))
     x
     }
 
-tbl_mapped_precursor <- fread(mapped_precursor_path) %>% as_tibble() %>% spaceless()
-tbl_qc_ce <- tbl_mapped_precursor %>%
-    filter(stringr::str_starts(Proteins, "QC")) %>%
-    mutate(pool_name = pool) %>%
-    select(pool_name, Scan_number, Proteins, Sequence, Charge, Modifications, Score, collision_energy) %>%
+tbl_mapped_precursor <- fread(mapped_precursor_path) %>%
+    as_tibble() %>%
+    spaceless() %>%
+    select(Scan_number, Proteins) %>%
+    filter(startsWith(Proteins, "QC_")) %>%
     distinct()
 
-fwrite(tbl_qc_ce, ce_output_path, append = TRUE)
+tbl_un_annotated_ce <- fread(un_annotated_ce_path) %>%
+    mutate(Scan_number = SCAN_NUMBER) %>%
+    merge(tbl_mapped_precursor) %>%
+    as_tibble() %>%
+    distinct()
+
+fwrite(tbl_un_annotated_ce, ce_output_path, append = TRUE)
