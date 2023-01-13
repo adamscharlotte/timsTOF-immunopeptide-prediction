cdbb7b47fe12a146b6705320e548ec75cb639d33
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-01-13 16:59:13 +0100

    We found some peptides that are overlapping across pools. Not in full length but in N-terminal truncation.

--- Annotation/test_split.R
+++ Annotation/test_split.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 library(data.table)
+library(readr)
+library(ggplot2)
+# install.packages("hrbrthemes")
+library(hrbrthemes)
+library(viridis)
 
 base <- "/Users/adams/Projects/300K/2022-library-run/"
-msms_path <- paste(base, "msms-txtx/", sep = "")
 meta_path <- paste(base, "Metadata/full-pool-sequence.txt", sep = "")
 map_path <- paste(base, "Metadata/full-meta-map.txt", sep = "")
+msms_path <- paste(base, "msms-txt/", sep = "")
+# meta_qc_path <- paste(base, "Metadata/qc-peptides.txt", sep = "")
+filter_path <- paste(base, "Annotation/total-scan-consensus/filtered/", sep = "") # nolint
 
 tbl_meta <- fread(meta_path) %>% as_tibble()
 tbl_meta %>%
@@ -51,9 +58,151 @@ validation_set %>% count(plate)
 validation_set %>%
     filter(pool_name %in% test_set$pool_name)
 
-# -------------------- WRITE THE POOL NAMES TO A TXT FILE --------------------
+# ---------------------------- TEST THE SETS --------------------------------
+spaceless <- function(x) {
+    colnames(x) <- gsub(" ", "_", colnames(x))
+    x
+    }
 
+setwd(msms_path)
+list_csv_files <- list.files(path = msms_path)
+df_msms <- readr::read_tsv(list_csv_files, id = "file_name")
 
+setwd(filter_path)
+list_csv_files <- list.files(path = filter_path)
+df_filtered <- readr::read_csv(list_csv_files, id = "file_name")
+
+tbl_filtered <- df_filtered %>%
+    mutate(pool_name = str_remove(file_name, ".csv"))
+
+tbl_map <- df_msms %>%
+    spaceless() %>%
+    select(Proteins, Scan_number) %>%
+    rename_with(toupper) %>%
+    mutate(Proteins = PROTEINS)
+
+tbl_filtered_mapped <- merge(tbl_filtered, tbl_map,
+    by.x = c("SCAN_NUMBER", "pool_name"),
+    by.y = c("SCAN_NUMBER", "PROTEINS"), all.x = TRUE) %>%
+    as_tibble()
+
+tbl_filtered_mapped %>% count(Proteins) %>% arrange(desc(n)) #%>% print(n=300)
+
+tbl_test <- tbl_filtered_mapped %>%
+    filter(pool_name %in% test_set$pool_name) %>%
+    filter(!is.na(Proteins))
+
+tbl_validation <- tbl_filtered_mapped %>%
+    filter(pool_name %in% validation_set$pool_name) %>%
+    filter(!is.na(Proteins))
+
+tbl_train <- tbl_filtered_mapped %>%
+    filter(!pool_name %in% validation_set$pool_name) %>%
+    filter(!pool_name %in% tbl_test$pool_name)
+
+nrow(tbl_test)
+nrow(tbl_validation)
+nrow(tbl_train)
+
+tbl_test %>% filter(OBS_SEQUENCE %in% tbl_validation$OBS_SEQUENCE)
+
+tbl_test_unique <- tbl_test %>%
+    filter(!OBS_SEQUENCE %in% tbl_train$OBS_SEQUENCE) %>%
+    filter(!OBS_SEQUENCE %in% tbl_validation$OBS_SEQUENCE)
+
+tbl_validation_unique <- tbl_validation %>%
+    filter(!OBS_SEQUENCE %in% tbl_train$OBS_SEQUENCE) %>%
+    filter(!OBS_SEQUENCE %in% tbl_test$OBS_SEQUENCE)
+
+tbl_test %>%
+    filter(OBS_SEQUENCE %in% tbl_train$OBS_SEQUENCE) %>%
+    filter(OBS_SEQUENCE == SEQUENCE) %>%
+    count(Proteins) %>%
+    arrange(desc(n))
+
+tbl_train %>%
+    filter(OBS_SEQUENCE %in% tbl_test$OBS_SEQUENCE) %>%
+    filter(OBS_SEQUENCE == SEQUENCE) %>%
+    count(Proteins) %>%
+    arrange(desc(n))
+
+tbl_validation %>%
+    filter(OBS_SEQUENCE %in% tbl_train$OBS_SEQUENCE) %>%
+    filter(OBS_SEQUENCE == SEQUENCE)
+
+c_overlap <- function(group1, group2) {
+    tbl_1 <- tbl_meta %>% filter(pool_name == group1)
+    tbl_2 <- tbl_meta %>%
+        filter(pool_name == group2) %>%
+        rename(pool_name2 = pool_name) %>%
+        rename(Sequence2 = Sequence)
+    tbl_overlap <- merge(tbl_1, tbl_2) %>%
+        as_tibble() %>%
+        filter(endsWith(Sequence, Sequence2))
+    return(nrow(tbl_overlap))
+}
+
+tbl_meta_map %>% count(plate)
+hla2_p2 <- tbl_meta_map %>%
+    filter(plate == "20220623_HLAII_p2") %>%
+    select(pool_name)
+
+hla2_p2_compare <- hla2_p2 %>%
+    rename(group1 = pool_name) %>%
+    merge(hla2_p2) %>%
+    as_tibble() %>%
+    filter(!group1 == pool_name)
+
+hla2_p2_overlap <- hla2_p2_compare %>%
+    rowwise() %>%
+    mutate(overlap = c_overlap(group1, pool_name))
+
+hla2_p2_overlap_B <- hla2_p2_compare %>%
+    rowwise() %>%
+    mutate(overlap = c_overlap(pool_name, group1))
+
+hla2_p2_overlap %>% arrange(desc(overlap))
+hla2_p2_overlap_B %>% arrange(desc(overlap))
+
+ggplot(hla2_p2_overlap, aes(group1, pool_name, fill = overlap)) +
+    geom_tile() +
+    scale_fill_viridis(discrete = FALSE) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+hla_p2_both_sides <- hla2_p2_overlap_B %>%
+    rename(pool_name_t = group1) %>%
+    rename(group1 = pool_name) %>%
+    rename(pool_name = pool_name_t) %>%
+    rbind(hla2_p2_overlap) %>%
+    distinct()
+
+8319 + 8287 + 78522
+8319/95128
+8287/95128
+78522/95128
+
+7818 + 7765  + 78522
+7818/94105
+7765/94105
+78522/94105
+
+94105/97216
+tbl_pool_peptides <- fread(meta_path) %>%
+    as_tibble() %>%
+    dplyr::rename(Proteins = pool_name) %>%
+    distinct()
+
+tbl_mapped <- merge(tbl_msms, tbl_pool_peptides, by = "Proteins") %>%
+    as_tibble()
+
+tbl_filtered <- tbl_mapped %>%
+    filter(endsWith(Sequence, obs_sequence)) %>%
+    filter(str_length(obs_sequence) <= str_length(Sequence)) %>%
+    group_by(obs_sequence) %>%
+    filter(str_length(Sequence) == min(str_length(Sequence))) %>%
+    ungroup()
+
+colnames(tbl_msms)
 # -------------------- WRITE THE POOL NAMES TO A TXT FILE --------------------
 test_set_t <- test_set$pool_name
 
