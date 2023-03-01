51803ca1360a4d80919eb1e9a7f59331edf33332
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-03-01 16:53:02 +0100

    Start of code development.

--- Annotation/scan_linear_calibrate.py
+++ Annotation/scan_linear_calibrate.py
@@ -36,29 +36,42 @@ def get_spectral_angle(intensities):
     arccos = np.arccos(product)
     return 1-2*arccos/np.pi
 
-parser = argparse.ArgumentParser()
-parser.add_argument('pool', type=str)					# Filename
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('pool', type=str)					# Filename
+# args = parser.parse_args()
 
-# pool = "TUM_HLA2_7"
-pool = args.pool
 base_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/" # nolint
-annot_path = base_path + "scan-consensus/annotated-40-ppm/" + pool + ".csv"
-cali_path = base_path + "scan-consensus/calibrated-linear-40-ppm/" + pool + ".csv"
+# annot_path = base_path + "total-scan-consensus/annotated-40-ppm/" + pool + ".csv"
+# cali_path = base_path + "total-scan-consensus/calibrated-linear-40-ppm/" + pool + ".csv"
 # ce_sa_path = base_path + "scan-consensus/spectral-angle/" + pool
+training_path = "/home/cadams/train_set.txt"
+
+with open(training_path, "r") as training_files:
+    training_lines = training_files.readlines()
+    training_list = []
+    for l in training_lines:
+        as_list = l.split(", ")
+        training_list.append(base_path + "total-scan-consensus/annotated-40-ppm/" + 
+            as_list[0].replace("\n", "") + ".csv")
+
+len(training_list)
+
+# Read CSV files from List
+training_df = pd.concat(map(pd.read_csv,training_list))
 
 full_df = pd.read_csv(annot_path)
 full_df.INTENSITIES = full_df.INTENSITIES.str.split(";").apply(lambda s: [float(x) for x in s])
 full_df.MZ = full_df.MZ.str.split(";").apply(lambda s: [float(x) for x in s])
 full_df.SEQUENCE_INT = full_df.SEQUENCE_INT.str.strip("][").str.split(", ").apply(lambda s: [int(x) for x in s])
 
-full_df.rename(columns = {"mean_CE": "ORIG_COLLISION_ENERGY"}, inplace=True)
+full_df.rename(columns = {"median_CE": "ORIG_COLLISION_ENERGY"}, inplace=True)
 # full_df.columns
 
 # Filter based on score
-col_filter = ['SCORE']
-full_df[col_filter] = full_df[full_df[col_filter] >= 70][col_filter]
+col_filter = ['PRECURSOR_CHARGE']
+full_df[col_filter] = full_df[full_df[col_filter] <= 3][col_filter]
 filtered_annot_df = full_df.dropna()
+print("How many PSMs with charge 4?", len(full_df.index) - len(filtered_annot_df.index))
 
 # -----------------------------------------------------------------------------
 
@@ -83,22 +96,23 @@ for charge, df_charge in grouped_charge_df:
     top_100_df = pd.concat([top_100_df for _ in CE_RANGE], axis=0)
     top_100_df["COLLISION_ENERGY"] = np.repeat(CE_RANGE, nrow)
     top_100_df.reset_index(inplace=True)
-    predictions = predictor.predict(sequences=top_100_df['MODIFIED_SEQUENCE'].values.tolist(),
-                                    charges=top_100_df["PRECURSOR_CHARGE"].values.tolist(),
-                                    collision_energies=top_100_df["COLLISION_ENERGY"].values/100.0,
-                                                        models=['Prosit_2020_intensity_hcd'],
-                                                        disable_progress_bar=True)
-    top_100_df['PREDICTED_INTENSITY'] = predictions['Prosit_2020_intensity_hcd']['intensity'].tolist()
-    top_100_df["SPECTRAL_ANGLE"] = top_100_df[['INTENSITIES','PREDICTED_INTENSITY']].apply(lambda x : get_spectral_angle(x), axis=1)
-    top_100_df["SPECTRAL_ANGLE"].fillna(0, inplace=True)
+predictions = predictor.predict(sequences=top_100_df['MODIFIED_SEQUENCE'].values.tolist(),
+                                charges=top_100_df["PRECURSOR_CHARGE"].values.tolist(),
+                                collision_energies=top_100_df["COLLISION_ENERGY"].values/100.0,
+                                                    models=['Prosit_2020_intensity_hcd'],
+                                                    disable_progress_bar=True)
+top_100_df['PREDICTED_INTENSITY'] = predictions['Prosit_2020_intensity_hcd']['intensity'].tolist()
+top_100_df["SPECTRAL_ANGLE"] = top_100_df[['INTENSITIES','PREDICTED_INTENSITY']].apply(lambda x : get_spectral_angle(x), axis=1)
+top_100_df["SPECTRAL_ANGLE"].fillna(0, inplace=True)
     # top_100_df.to_csv(ce_sa_path + '_' + str(charge) + '.csv')
-    groups = top_100_df.groupby(by=['ORIG_COLLISION_ENERGY', "COLLISION_ENERGY", "MASS"])["SPECTRAL_ANGLE"].mean()
-    groups_2 = groups.reset_index()
-    ids = groups_2.groupby(['ORIG_COLLISION_ENERGY'])['SPECTRAL_ANGLE'].transform(max) == groups_2['SPECTRAL_ANGLE']
-    calib_group = groups_2[ids]
-    calib_group['delta_collision_energy'] = calib_group['COLLISION_ENERGY'] - calib_group['ORIG_COLLISION_ENERGY']
+groups = top_100_df.groupby(by=['ORIG_COLLISION_ENERGY', "COLLISION_ENERGY", "MASS"])["SPECTRAL_ANGLE"].mean()
+groups_2 = groups.reset_index()
+ids = groups_2.groupby(['ORIG_COLLISION_ENERGY'])['SPECTRAL_ANGLE'].transform(max) == groups_2['SPECTRAL_ANGLE']
+calib_group = groups_2[ids]
+calib_group['delta_collision_energy'] = calib_group['COLLISION_ENERGY'] - calib_group['ORIG_COLLISION_ENERGY']
+linear_model.LinearRegression(calib_group[['MASS','delta_collision_energy']].to_numpy(),
+    fit_intercept=True, copy_X=True, n_jobs=None, positive=False)
     ce_alignment = calib_group[['delta_collision_energy', 'MASS']]
-
     df_charge['aligned_collision_energy'] = df_charge['ORIG_COLLISION_ENERGY'] + best_ce
     appended_data.append(df_charge)
 
