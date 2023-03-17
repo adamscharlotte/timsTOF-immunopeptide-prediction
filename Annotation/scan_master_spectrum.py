fc45569e40b52db7c7054a8a8d67b62f1dcaa58b
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-03-17 18:32:21 +0100

    Adjust the code for the complete set of data.

--- Annotation/scan_master_spectrum.py
+++ Annotation/scan_master_spectrum.py
@@ -21,7 +21,8 @@ args = parser.parse_args()
 # pool = "TUM_first_pool_5"
 pool = args.pool
 
-base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
+# base_path = "/Users/adams/Projects/300K/2022-library-run/Annotation/"
+base_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/" # nolint
 un_annot_path = base_path + "total-scan-consensus/filtered/" + pool + ".csv"
 sum_path = base_path + "total-scan-consensus/summed-40-ppm/" + pool + ".csv"
 
@@ -34,8 +35,8 @@ un_annot_df.combined_MZ = un_annot_df.combined_MZ.str.split(";").apply(lambda s:
 def binning(inp, ignoreCharges):
     ms = MasterSpectrum()
     ms.load_from_tims(inp, ignoreCharges)
-    ms.export_to_csv(base_path + "scan-consensus/tmp-MasterSpectrum/24102022.csv")
-    comb_ms = pd.read_csv(base_path + "scan-consensus/tmp-MasterSpectrum/24102022.csv")
+    ms.export_to_csv(base_path + "total-scan-consensus/tmp-MasterSpectrum/24102022.csv")
+    comb_ms = pd.read_csv(base_path + "total-scan-consensus/tmp-MasterSpectrum/24102022.csv")
     scan = inp["SCAN_NUMBER"]
     comb_ms["SCAN_NUMBER"] = scan
     comb_ms = comb_ms.drop(columns=["counts", "left border", "right border", "start_mz", "ms1_charge", "rel_intensity_ratio", "counts_ratio"])
@@ -53,15 +54,32 @@ un_annot_df_combined.rename(columns = {"CHARGE": "PRECURSOR_CHARGE"}, inplace=Tr
 un_annot_df_combined["REVERSE"].fillna(False, inplace=True)
 un_annot_df_combined["REVERSE"].replace("+", True, inplace=True)
 un_annot_df_combined["MODIFIED_SEQUENCE"] = maxquant_to_internal(un_annot_df_combined["MODIFIED_SEQUENCE"].to_numpy())
-un_annot_df_combined["SEQUENCE"] = internal_without_mods(un_annot_df_combined["MODIFIED_SEQUENCE"])
-un_annot_df_combined["PEPTIDE_LENGTH"] = un_annot_df_combined["SEQUENCE"].apply(lambda x: len(x))
+un_annot_df_combined["OBS_SEQUENCE"] = internal_without_mods(un_annot_df_combined["MODIFIED_SEQUENCE"])
+un_annot_df_combined["PEPTIDE_LENGTH"] = un_annot_df_combined["OBS_SEQUENCE"].apply(lambda x: len(x))
 un_annot_df_combined = un_annot_df_combined.drop(columns=["combined_INTENSITIES", "combined_MZ"])
 un_annot_df_combined.rename(columns = {"mz": "MZ"}, inplace=True)
 un_annot_df_combined.rename(columns = {"intensity": "INTENSITIES"}, inplace=True)
 
+# ------------- Sequence int observed sequence -------------
 generator_sequence_numeric = parse_modstrings(list(un_annot_df_combined["MODIFIED_SEQUENCE"].values), ALPHABET, translate=True)
 enum_gen_seq_num = enumerate(generator_sequence_numeric)
 array = np.zeros((len(list(un_annot_df_combined["MODIFIED_SEQUENCE"].values)),30), dtype=np.uint8)
+for i, sequence_numeric in enum_gen_seq_num:
+    if len(sequence_numeric) > 30:
+        if filter:
+            pass # don"t overwrite 0 in the array that is how we can differentiate
+        else:
+            raise Exception(f"The Sequence {sequence_numeric}, has {len(sequence_numeric)} Amino Acids."
+                        f"The maximum number of amino acids allowed is {C.SEQ_LEN}")
+    else:
+        array[i, 0:len(sequence_numeric)] = sequence_numeric
+
+un_annot_df_combined["OBS_SEQUENCE_INT"] = array.tolist()
+
+# ------------- Sequence int full length sequence -------------
+generator_sequence_numeric = parse_modstrings(list(un_annot_df_combined["SEQUENCE"].values), ALPHABET, translate=True)
+enum_gen_seq_num = enumerate(generator_sequence_numeric)
+array = np.zeros((len(list(un_annot_df_combined["SEQUENCE"].values)),30), dtype=np.uint8)
 for i, sequence_numeric in enum_gen_seq_num:
     if len(sequence_numeric) > 30:
         if filter:
