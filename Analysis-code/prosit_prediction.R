96395acd2c22f5ccd64bbf726881f584683108d5
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-03-17 18:20:57 +0100

    Look at the SA.

--- Analysis-code/prosit_prediction.R
+++ Analysis-code/prosit_prediction.R
@@ -1,41 +1,29 @@
 library(tidyverse)
 library(data.table)
 
-pool <- args[1]
-precursor <- args[2]
+base_path <- "/Users/adams/Projects/300K/2022-library-run/Annotation/total-scan-consensus/spectral-angle-comparison" # nolint
 
-pool <- "TUM_HLA2_31"
+csv_names <- list.files(path = base_path, pattern = "*.csv", full.names = TRUE)
+csv_names2 <- data.frame(month = csv_names, id = as.character(1:length(csv_names)))
 
-base_path <- "/Users/adams/Projects/300K/2022-library-run/"
-meta_path <- paste(base_path, "Metadata/full-meta-map.txt", sep = "")
-top_path <- paste(base_path, "Metadata/top-identifications.txt", sep = "")
-sum_path <- paste(base_path, "Annotation/precursor-consensus/summed", sep = "")
+data <- csv_names %>%
+    lapply(read_csv) %>% # read all the files at once
+    bind_rows(.id = "id") %>% # bind all tables into one object, and give id for each
+    left_join(csv_names2) # join month column created earlier
 
-tbl_meta <- fread(meta_path) %>%
-    as_tibble() %>%
-    mutate(folder_names = substring(folder_names, 1, nchar(folder_names) - 2))
+data_1 <- data %>%
+    distinct() %>%
+    filter(PRECURSOR_CHARGE == 1)
 
-tbl_top <- fread(top_path) %>%
-    as_tibble() %>%
-    unite(pool_precursor, V1:V2, sep = "_")
+data_2 <- data %>%
+    distinct() %>%
+    filter(PRECURSOR_CHARGE == 2)
 
-setwd(sum_path)
-files <- dir(pattern = "*.csv")
-scans_txt <- files %>%
-    map(read_csv) %>%        # read in all the files individually, using
-                            # the function read_csv() from the readr package
-    reduce(rbind)           # reduce with rbind into one dataframe
+data_3 <- data %>%
+    distinct() %>%
+    filter(PRECURSOR_CHARGE == 3)
 
-tbl_sum <- scans_txt %>%
-    merge(tbl_meta, by.x = "RAW_FILE", by.y = "folder_names") %>%
-    as_tibble() %>%
-    select(pool_name, PRECURSOR, everything()) %>%
-    unite(pool_precursor, pool_name:PRECURSOR, sep = "_") %>%
-    filter(pool_precursor %in% tbl_top$pool_precursor) %>%
-    select(MODIFIED_SEQUENCE, COLLISION_ENERGY, PRECURSOR_CHARGE)
-
-
-prosit_path <- paste(base_path, "Metadata/top-identifications-prosit.csv", sep = "")
-write_csv(tbl_sum, prosit_path)
-
-# 6A760C626CB0508BE6F08C49B34094FD
+mean(data$SPECTRAL_ANGLE)
+mean(data_1$SPECTRAL_ANGLE)
+mean(data_2$SPECTRAL_ANGLE)
+mean(data_3$SPECTRAL_ANGLE)
