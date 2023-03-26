59aa2ef72a08c65cf2f1c81e97a3cd6c2f25255e
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-03-26 17:56:04 +0200

    Extract the pasef files from the .d folders from PXD038782 d folders.

--- Annotation/extract_pasef.py
+++ Annotation/extract_pasef.py
@@ -10,12 +10,14 @@ pool = args.pool
 
 # pool = "TUM_first_pool_124"
 # raw_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/Analysis/" + pool + "_01_01/TIMS-30min-R1-TIMS_unspecific/230719_f1-" + pool + "_01_01-TIMS-30min-R1.d"
-raw_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/Analysis/" + pool + "_01_01/TIMS-30min-R1-TIMS_semitryptic/230719_f1-" + pool + "_01_01-TIMS-30min-R1.d"
+# raw_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/Analysis/" + pool + "_01_01/TIMS-30min-R1-TIMS_semitryptic/230719_f1-" + pool + "_01_01-TIMS-30min-R1.d"
+raw_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/PXD038782-comparison/d-folder/" + pool + ".d"
 tdf_path = raw_path + "/analysis.tdf"
 con = sqlite3.connect(tdf_path)
 cur = con.cursor()
 pasef_df = pd.read_sql_query("SELECT * from PasefFrameMsMsInfo", con)
 con.close()
 
-output_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/extract-pasef/" + pool + ".csv"
+# output_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Annotation/extract-pasef/" + pool + ".csv"
+output_path = "/media/kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/PXD038782-comparison/extract-pasef/" + pool + ".csv"
 pasef_df.to_csv(output_path)
