5a834fb33d6f6ef0dfbddfc8c4e7199880b41273
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-04-04 14:56:05 +0200

    Convert Thermo raw files to mgf files.

---
+++ Plots/Mirror/thermo_raw_mgf.sh
@@ -0,0 +1,3 @@
+mono /Users/adams/Projects/300K/prosit_io/prosit_io/raw/utils/ThermoRawFileParser/ThermoRawFileParser.exe -i=/Users/adams/Downloads/02446d_GD1-TUM_HLA_133_01_01-3xHCD-1h-R4.raw -o=/Users/adams/Downloads/ -f=0
+
+mono /Users/adams/Projects/300K/prosit_io/prosit_io/raw/utils/ThermoRawFileParser/ThermoRawFileParser.exe -i=/Users/adams/Downloads/02445d_BB3-TUM_HLA_15_01_01-3xHCD-1h-R4.raw -o=/Users/adams/Downloads/ -f=0
