44e842f3ffffd775a510bec660526b486e92f38b
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-06-13 10:32:12 +0200

    Plot CDF

--- Plots/cdf_plot.py
+++ Plots/cdf_plot.py
@@ -2,22 +2,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 penguins = sns.load_dataset("penguins")
-sns.ecdfplot(data=penguins, x="flipper_length_mm")
-sns.ecdfplot(data=penguins, y="flipper_length_mm")
-sns.ecdfplot(data=penguins.filter(like="bill_", axis="columns"))
+# sns.ecdfplot(data=penguins, x="flipper_length_mm")
+# sns.ecdfplot(data=penguins, y="flipper_length_mm")
+# sns.ecdfplot(data=penguins.filter(like="bill_", axis="columns"))
 
-plt.figure()
 sns.set_context("talk")
 width = 6.5
 height = 6.0
 # width = 4
 # height = 5
-
+# penguins.species.drop_duplicates()
 fig, ax = plt.subplots(figsize=(width, height))
 
-ax = sns.ecdfplot(data=penguins, x="bill_length_mm", hue="species")
-ax.legend(loc="center right", bbox_to_anchor=(1.4, 0.5))#,fancybox=True, ncol=1)
+ax = sns.ecdfplot(data=penguins, x="bill_length_mm", hue="species", palette=["#CDEAC0", "#f33b16", "#7A8DB3"])
+ax.legend(loc="center right", bbox_to_anchor=(1.8, 0.5))#,fancybox=True, ncol=1)
 sns.despine()
+ax.grid(False, which="both")
+plt.xlabel('Affinity score')
 
 plt.show()
 
