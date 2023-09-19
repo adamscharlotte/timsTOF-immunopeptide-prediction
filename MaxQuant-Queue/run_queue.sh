c9603c73a0af57fe91e48f47a086cdce5bf4a494
adamscharlotte <charlotte.adams@student.uantwerpen.be>
2023-09-19 11:59:52 +0200

    Remove unused folders

--- MaxQuant-Queue/run_queue.sh
+++
@@ -1,4 +0,0 @@
-
-
-# Go to Z:\\\\internal_projects\\\\active\\\\ProteomeTools\\\\ProteomeTools\\\\External_data\\\\Bruker\\\\UA-TimsTOF-300K
-# kusterlab/internal_projects/active/ProteomeTools/ProteomeTools/External_data/Bruker/UA-TimsTOF-300K/Queue
